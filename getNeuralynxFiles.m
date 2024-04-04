e6fd98b729ec70c4f2ba92ba1b716a25a869917c
Xin <michaelniki1988@gmail.com>
2024-04-03 18:57:51 -0700

    add config, ignore files

---
+++ getNeuralynxFiles.m
@@ -0,0 +1,24 @@
+function filenames = getNeuralynxFiles(directory, fileExt, ignoreFilesWithSizeBelow)
+
+if nargin < 3 || isempty(ignoreFilesWithSizeBelow)
+    ignoreFilesWithSizeBelow = 16384;
+end
+
+% load configure variables:
+run('config.m')
+
+d = dir([directory, filesep, '*', fileExt]);
+d([d.isdir]) = [];
+filenames = {d.name};
+
+if nargin == 3
+    filesizes = [d.bytes];
+    filenames = filenames(filesizes > ignoreFilesWithSizeBelow + 1);  
+end
+
+filenames = filenames(:);
+
+keep = cellfun(@(x) ~any(cellfun(@(p) ~isempty(regexp(x, p, 'once')), IGNORE_FILES)), filenames);
+filenames = filenames(keep);
+
+end
