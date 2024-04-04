98243d55c0df45979aef0f7d128191cf399df4b0
Xin <michaelniki1988@gmail.com>
2024-04-03 19:07:45 -0700

    fix ignore file pattern

--- config.m
+++ config.m
@@ -4,15 +4,15 @@ MACRO_FILE_PATTERN = '^[RL].*[0-9]*.ncs';
 MICRO_FILE_PATTERN = '^G[A-D]-*.ncs';
 
 IGNORE_FILES = {
-    'A1*.ncs';
-    'A2*.ncs';
-    'C3*.ncs';
-    'C4*.ncs';
-    'EMG*.ncs';
-    'EOG*.ncs';
-    'Analogue2*.ncs';
-    'Ez*.ncs';
-    'HR_Ref*.ncs';
-    'MICROPHONE*.ncs';
-    'PZ*.ncs';
+    'A1.*\.ncs';
+    'A2.*\.ncs';
+    'C3.*\.ncs';
+    'C4.*\.ncs';
+    'EMG.*\.ncs';
+    'EOG.*\.ncs';
+    'Analogue2.*\.ncs';
+    'Ez.*\.ncs';
+    'HR_Ref.*\.ncs';
+    'MICROPHONE.*\.ncs';
+    'PZ.*\.ncs';
 };
