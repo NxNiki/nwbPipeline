a8b148fe569db0886a8976d20be85e775eb629f8
Xin <michaelniki1988@gmail.com>
2024-04-04 17:48:05 -0700

    save lfpTimeStamps for each segment (_001, _002, etc)

--- unpackData.m
+++ unpackData.m
@@ -1,6 +1,6 @@
 function unpackData(inFileNames, outFileNames, outFilePath, verbose)
-% unpackData(inFileNames, outFilePath, verbose): read neuralynx file and 
-% save to .mat files. 
+% unpackData(inFileNames, outFilePath, verbose): read neuralynx file and
+% save to .mat files.
 
 % inFileName: datatable(m, 1). '.ncs' files for one experiment. Should have same
 % timestamps.
@@ -18,35 +18,46 @@ end
 if verbose
     fprintf('unpack the first file: %s with timestamp information...\n', inFileNames{1});
 end
-computeTS = true;
+
 % TO DO: probably don't want to hard code timestamp file name.
-timestampFileName = 'lfpTimeStamps.mat';
-[data, timeStamps, samplingInterval, ~] = Nlx_readCSC(inFileNames{1}, computeTS, outFilePath);
-
-num_samples = length(data);
-time0 = 0; 
-timeend = (num_samples-1) * (samplingInterval/1000); % in seconds
-
-[~, filename, ~] = fileparts(outFileNames{1});
-save(fullfile(outFilePath, [filename, '.mat']), 'data', 'samplingInterval', 'time0', 'timeend', '-v7.3');
-save(fullfile(outFilePath, timestampFileName), 'timeStamps', 'samplingInterval', 'time0', 'timeend', '-v7.3');
+timestampFileName = 'lfpTimeStamps';
 
 % unpack the remainning files without computing the timestamp:
-computeTS = false;
 for i = 2:length(inFileNames)
     inFileName = inFileNames{i};
-    [~, filename, ~] = fileparts(outFileNames{i});
-    outFileName = fullfile(outFilePath, [filename, '.mat']);
+    [~, outFileName, ~] = fileparts(outFileNames{i});
+    outFileName = fullfile(outFilePath, [outFileName, '.mat']);
+    suffix = regexp(outFileName, '(?<=_)\d{3}.mat', 'match', 'once');
 
     if verbose
         fprintf('unpack: %s\n', inFileName);
     end
 
-    [signal, ~ , samplingInterval, ~] = Nlx_readCSC(inFileName, computeTS, outFilePath);
+    timestampFullFile = fullfile(outFilePath, [timestampFileName, suffix]);
+    if ~exist(timestampFullFile, "file")
+        computeTS = true;
+
+    else
+        computeTS = false;
+    end
+    [~, timeStamps, samplingInterval, ~] = Nlx_readCSC(inFileNames{1}, computeTS, outFilePath);
+    num_samples = length(data);
+    time0 = 0;
+    timeend = (num_samples-1) * (samplingInterval/1000); % in seconds
+
     matobj = matfile(outFileName, 'Writable', true);
     matobj.samplingInterval = samplingInterval;
     matobj.data = signal;
     matobj.time0 = time0;
     matobj.timeend = timeend;
+
+    if computeTS
+        matobj = matfile(timestampFullFile, Writable=true);
+        matobj.timeStamps = timeStamps;
+        matobj.samplingInterval = samplingInterval;
+        matobj.time0 = time0;
+        matobj.timeend = timeend;
+    end
+
 end
 
