839ba99da12b32c16b23bde6dc8a082d42ecd59a
Xin <michaelniki1988@gmail.com>
2024-04-09 15:33:50 -0700

    add spike detection

--- run_unpackNeuralynx.m
+++ run_unpackNeuralynx.m
@@ -3,13 +3,13 @@
 % silicon.
 
 
-expId = 5;
-filePath = '/Volumes/DATA/NLData/D570/EXP5_Movie_24_Sleep/2024-01-27_00-01-35';
-outFilePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm';
+% expId = 5;
+% filePath = '/Volumes/DATA/NLData/D570/EXP5_Movie_24_Sleep/2024-01-27_00-01-35';
+% outFilePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm';
 
-% expId = 2;
-% filePath = '/Volumes/DATA/NLData/D555/EXP2_Screening/2022-07-31_12-46-04';
-% outFilePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/555_Screening';
+expId = 2;
+filePath = '/Volumes/DATA/NLData/D555/EXP2_Screening/2022-07-31_12-46-04';
+outFilePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/555_Screening';
 
 % 0: will remove all previous unpack files.
 % 1: skip existing files.
--- spikeDetection.m
+++ spikeDetection.m
@@ -1,47 +1,82 @@
-function [outputArg1,outputArg2] = spikeDetection(cscFiles, outputPath, skipExist)
+function spikeDetection(cscFiles, timestampFiles, outputPath, experimentName, skipExist)
 %spikeDetection Summary of this function goes here
 %   cscFiles cell(m, n). m: number of channels. n: number of files in each
-%   channel. 
+%   channel.
 
-% 
-
-spikeFilename = ['spikes_', regexp(cscFiles{1}, '.*(?=_\d+\.mat)', 'match', 'once'), '.mat'];
-nSegments = length(cscFiles);
-param = set_parameters();
-maxAmp = 500;
-
-for i = nSegments:-1:1
-    [signals{i}, samplingInterval] = readCSC(cscFiles{i});
-    
-    param.sr = seconds(1)/samplingInterval;
-    param.ref = floor(1.5 * param.sr/1000); 
-    [thr(i), outputStruct(i)] = getDetectionThresh(signals{i}, param, maxAmp);
-
-    timestamps{i} = readTimestamps(filename);
+if nargin < 4 || isempty(experimentName)
+    experimentName = repmat({''}, length(timestampFiles), 1);
 end
 
-thr_all = min(thr);
-% it shouldn't go less than 18. If it does, it probably found a file with a long stretch of flat, and will then find millions of spikes in the non-flat section.
-thr_all = max(thr_all, 18); 
-maxThr = max([outputStruct.thrmax]);
-common_noise_std_detect = min([outputStruct.noise_std_detect]);
-common_noise_std_sorted = min([outputStruct.noise_std_sorted]);
-
-for i = 1:nSegments
-    fprintf('.');
-    outputStruct(i).thrmax = maxThr;
-    outputStruct(i).noise_std_detect = common_noise_std_detect;
-    outputStruct(i).noise_std_sorted = common_noise_std_sorted;
-    outputStruct(i).thr = thr_all;
-
-    [spikes, thr, index, spikeCodes, spikeHist, spikeHistPrecise, ~, ~] = amp_detect_AS(signals{i}, par, maxAmp, timestamps{i}, duration(i), thr_all, outputStruct(i));
-    hasSpikesAll{i} = spikeHist;
-    hasSpikesPreciseAll{i} = spikeHistPrecise;
-    timestamps{i} = timestamps{i}(index); % convert index into msec
+if nargin < 5
+    skipExist = false;
 end
 
-save_aux(fullfile(outputPath, spikeFilename), spikes, index, thr, spikeCodes, spikeHist, spikeHistPrecise)
 
+if ~exist(outputPath, "dir")
+    mkdir(outputPath);
+elseif ~skipExist
+    % create an empty dir to avoid not able to resume with unprocessed
+    % files in the future if this job fails. e.g. if we have 10 files
+    % processed in t1, t2 stops with 5 files processed, we cannot start
+    % with the 6th file in t3 as we have 10 files saved.
+    rmdir(outputPath, 's');
+    mkdir(outputPath);
+end
+
+nSegments = length(timestampFiles);
+for j = nSegments:-1:1
+    [timestamps{j}, dur] = readTimestamps(timestampFiles{j});
+    duration(j) = seconds(dur);
+end
+
+for i = 1: size(cscFiles, 1)
+    channelFiles = cscFiles(i,:);
+    spikeFilename = fullfile(outputPath, ['spikes_', regexp(channelFiles{1}, '.*(?=_\d+\.mat)', 'match', 'once'), '.mat']);
+
+    % TO DO: check file completeness:
+    if exist(spikeFilename, "file") && skipExist
+        continue
+    end
+
+    param = set_parameters();
+    maxAmp = 500;
+
+    for j = nSegments:-1:1
+        [signals{j}, samplingInterval] = readCSC(channelFiles{j});
+
+        param.sr = seconds(1)/samplingInterval;
+        param.ref = floor(1.5 * param.sr/1000);
+        [thr(j), outputStruct(j)] = getDetectionThresh(signals{j}, param, maxAmp);
+    end
+
+    thr_all = min(thr);
+    % it shouldn't go less than 18. If it does, it probably found a file with a long stretch of flat, and will then find millions of spikes in the non-flat section.
+    thr_all = max(thr_all, 18);
+    maxThr = max([outputStruct.thrmax]);
+    common_noise_std_detect = min([outputStruct.noise_std_detect]);
+    common_noise_std_sorted = min([outputStruct.noise_std_sorted]);
+
+    for j = nSegments:-1:1
+        fprintf('.');
+        outputStruct(j).thrmax = maxThr;
+        outputStruct(j).noise_std_detect = common_noise_std_detect;
+        outputStruct(j).noise_std_sorted = common_noise_std_sorted;
+        outputStruct(j).thr = thr_all;
+
+        [spikes{j}, thr, index, spikeCodes{j}, spikeHist{j}, spikeHistPrecise{j}, ~, ~] = amp_detect_AS(signals{j}, param, maxAmp, timestamps{j}, duration(j), thr_all, outputStruct(j));
+        spikeTimestamps{j} = timestamps{j}(index);
+        spikeCodes{j}.ExpName = experimentName{j};
+    end
+
+    matobj = matfile(spikeFilename, 'Writable', true);
+    matobj.spikes = [spikes{:}];
+    matobj.spikeTimestamps = [spikeTimestamps{:}];
+    matobj.thr = thr;
+    matobj.spikeCodes = vertcat(spikeCodes{:});
+    matobj.spikeHist = [spikeHist{:}];
+    matobj.spikeHistPrecise = [spikeHistPrecise{:}];
+
+end
 end
 
 
@@ -54,10 +89,11 @@ samplingInterval = seconds(matObj.samplingInterval);
 
 end
 
-function timestamps = readTimestamps(filename)
+function [timestamps, duration] = readTimestamps(filename)
 
 tsFileObj = matfile(filename);
 ts = tsFileObj.timeStamps;
 timestamps = ts(:)';
+duration = tsFileObj.timeend - tsFileObj.time0;
 
 end
