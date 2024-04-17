de5744f456edc06c6020739c3794ab89f7342c1b
Xin <michaelniki1988@gmail.com>
2024-04-17 13:33:24 -0700

    check spike and raw signals

--- scripts/test_lfp.m
+++ scripts/test_lfp.m
@@ -1,11 +1,20 @@
 
 
-cscfile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/CSC17.mat'};
-spikefile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/times_CSC17.mat'};
-timestampfile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/lfpTimeStamps.mat'};
+% cscfile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/CSC17.mat'};
+% spikefile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/times_CSC17.mat'};
+% timestampfile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/lfpTimeStamps.mat'};
+
+
+% 569 screening 1 (Exp 2) CSC35 Unit 2 RA (1/1)
+% !!! patient 569 has mis match in signal and spike time stamps...
+cscfile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/569_Screening/Experiment2/CSC_data/CSC35.mat'};
+spikefile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/569_Screening/Experiment2/CSC_data/times_CSC35.mat'};
+timestampfile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/569_Screening/Experiment2/CSC_data/lfpTimeStamps.mat'};
+
+
 % timestamps in this file is 0 based which is different from new data with
 % nwbpipeline.
-microLFPPath = '/Users/XinNiuAdmin/Documents/NWBTest/output/Screening/566_Screening/lfp_test';
+microLFPPath = '/Users/XinNiuAdmin/Documents/NWBTest/output/Screening/569_Screening/lfp_test';
 
 
 skipExist = 0; 
