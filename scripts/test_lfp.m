b24aedff500240a35e477b0753633b595a323fc2
Xin <michaelniki1988@gmail.com>
2024-04-17 10:04:59 -0700

    plot LFP

--- scripts/test_lfp.m
+++ scripts/test_lfp.m
@@ -3,8 +3,11 @@
 cscfile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/CSC17.mat'};
 spikefile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/times_CSC17.mat'};
 timestampfile = {'/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/566_Screening/Experiment2/CSC_data/lfpTimeStamps.mat'};
-
+% timestamps in this file is 0 based which is different from new data with
+% nwbpipeline.
 microLFPPath = '/Users/XinNiuAdmin/Documents/NWBTest/output/Screening/566_Screening/lfp_test';
+
+
 
 skipExist = 0; 
 saveRawSignal = true;
