b867b46ba485e06540116c281bc51285be84a915
Xin <michaelniki1988@gmail.com>
2024-04-30 11:53:44 -0700

    run screening (dependencies not migrated)

--- scripts/run_screening.m
+++ scripts/run_screening.m
@@ -1,21 +1,63 @@
+clear
 
-patient = 569;
-exp = 2;
+patient = 1720;
+exp = 1;
 
-trialFolder = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/569_Screening/Experiment2/CSC_data';
-imageDirectory = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/569_Screening/Experiment2/Screening_1/trial1';
+trialFolder = sprintf('/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/%d_Screening/Experiment%d/CSC_data', patient, exp);
+imageDirectory = sprintf('/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/%d_Screening/Experiment%d/Screening_1/trial1', patient, exp);
 
 addpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/DataPipeline_Screening'));
 rmpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/DataPipeline_Screening/Utilities/chronux'));
 
+%% unpack data:
+
+dataFolder='/Volumes/DATA/NLData/i720R/720-098_UCLA_Screening1/2024-04-29_14-41-45';
+
+if ~exist(trialFolder, "dir")
+    mkdir(trialFolder)
+end
+
+start = 97;
+endIdx = 112;
+
+computeTS = 1;
+[data,timeStamps,samplingInterval,chNum] = Nlx_readCSC(fullfile(dataFolder, ['PDes' num2str(start) '.ncs']), computeTS);
+time0 = timeStamps(1); 
+timeend = timeStamps(end);
+computeTS = 0;
+
+parfor i=start + 1: endIdx
+    fprintf(['processing file: %d, ' fullfile(dataFolder, ['PDes' num2str(i) '.ncs'])], i);
+    %if ~exist(['CSC' num2str(i) '.mat'], 'file')
+
+    [data,~,samplingInterval,chNum] = Nlx_readCSC(fullfile(dataFolder, ['PDes' num2str(i) '.ncs']), computeTS);
+    data = data';
+    save_aux(i, data, time0, timeend, samplingInterval, trialFolder);
+ %   end
+end
+ 
+save(fullfile(trialFolder, 'lfpTimeStamps.mat'), 'time0', 'timeend', 'timeStamps', 'samplingInterval'); 
+
+%%
+
+automaticScreeningAnalyze5(trialFolder);
+
+%%
+updateChannelMetaData(patient, exp)
+[clusterCharacteristics] = calculateClusterCharacteristics(patient, exp, 1, trialFolder);
 
 
 %%
-automaticScreeningAnalyze5(trialFolder);
-updateChannelMetaData(patient, exp)
-[clusterCharacteristics] = calculateClusterCharacteristics(patient, exp, 1);
 
 rasters_by_unit(patient, exp, imageDirectory, 1, 0)
 rasters_by_unit(patient, exp, imageDirectory, 0, 0)
 rasters_by_image(patient, exp, imageDirectory, 0);
 
+%%
+% rasters_by_unit(patient, exp, imageDirectory, 1, 1)
+% rasters_by_unit(patient, exp, imageDirectory, 0, 1)
+
+
+function save_aux(i, data, time0, timeend, samplingInterval, outputDir)
+    save(fullfile(outputDir, ['CSC' num2str(i) '.mat']), 'data', 'time0', 'timeend', 'samplingInterval');
+end
