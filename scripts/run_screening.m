897c0397b9cf472dbdcee77ea988cd18aae22a59
Xin <michaelniki1988@gmail.com>
2024-05-02 12:17:48 -0700

    add video screening

--- scripts/run_screening.m
+++ scripts/run_screening.m
@@ -1,10 +1,10 @@
 clear
 
 patient = 1720;
-exp = 1;
+exp = 2;
 
 trialFolder = sprintf('/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/%d_Screening/Experiment%d/CSC_data', patient, exp);
-imageDirectory = sprintf('/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/%d_Screening/Experiment%d/Screening_1/trial1', patient, exp);
+imageDirectory = sprintf('/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/%d_Screening/Experiment%d/Screening_2/trial1', patient, exp);
 
 addpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/DataPipeline_Screening'));
 rmpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/DataPipeline_Screening/Utilities/chronux'));
@@ -17,16 +17,20 @@ if ~exist(trialFolder, "dir")
     mkdir(trialFolder)
 end
 
-start = 97;
-endIdx = 112;
-
 computeTS = 1;
-[data, timeStamps, samplingInterval, chNum] = Nlx_readCSC(fullfile(dataFolder, ['PDes' num2str(start) '.ncs']), computeTS);
+[data, timeStamps, samplingInterval, chNum] = Nlx_readCSC(fullfile(dataFolder, ['PDes' num2str(1) '.ncs']), computeTS);
 time0 = timeStamps(1); 
 timeend = timeStamps(end);
+
+save_aux(1, data, time0, timeend, samplingInterval, trialFolder);
+save(fullfile(trialFolder, 'lfpTimeStamps.mat'), 'time0', 'timeend', 'timeStamps', 'samplingInterval'); 
+
+%%
+start = 97;
+endIdx = 112;
 computeTS = 0;
 
-parfor i=start + 1: endIdx
+parfor i=start: endIdx
     fprintf(['processing file: %d, ' fullfile(dataFolder, ['PDes' num2str(i) '.ncs']), '\n'], i);
     %if ~exist(['CSC' num2str(i) '.mat'], 'file')
 
@@ -35,26 +39,43 @@ parfor i=start + 1: endIdx
     save_aux(i, data, time0, timeend, samplingInterval, trialFolder);
  %   end
 end
- 
-save(fullfile(trialFolder, 'lfpTimeStamps.mat'), 'time0', 'timeend', 'timeStamps', 'samplingInterval'); 
+
+disp('unpacking finished!')
 
 %%
 
 automaticScreeningAnalyze5(trialFolder);
 
 %%
+% use PDM to add patient and session before proceeding with the following
+% steps:
+
+
 updateChannelMetaData(patient, exp)
 [clusterCharacteristics] = calculateClusterCharacteristics(patient, exp, 1, trialFolder);
+[clusterCharacteristics] = calculateClusterCharacteristics_video(patient, exp, 1, imageDirectory);
 
+%%
+
+command = ['chmod -R 775 ', trialFolder];
+system(command)
+
+patientInfoFolder = ' /Users/XinNiuAdmin/HoffmanMount/data/PATIENT_DATABASE/Patient1720';
+command = ['chmod -R 775 ', patientInfoFolder];
+system(command)
 
 %%
 
 rasters_by_unit(patient, exp, imageDirectory, 1, 0)
+rasters_by_unit(patient, exp, imageDirectory, 0, 0)
 
 rasters_by_image(patient, exp, imageDirectory, 0);
 
 %%
-rasters_by_unit(patient, exp, imageDirectory, 0, 0)
+
+rasters_by_unit_video(patient, exp, imageDirectory, 1, 0)
+rasters_by_unit_video(patient, exp, imageDirectory, 0, 0)
+
 % rasters_by_unit(patient, exp, imageDirectory, 1, 1)
 % rasters_by_unit(patient, exp, imageDirectory, 0, 1)
 
@@ -62,3 +83,4 @@ rasters_by_unit(patient, exp, imageDirectory, 0, 0)
 function save_aux(i, data, time0, timeend, samplingInterval, outputDir)
     save(fullfile(outputDir, ['CSC' num2str(i) '.mat']), 'data', 'time0', 'timeend', 'samplingInterval');
 end
+
