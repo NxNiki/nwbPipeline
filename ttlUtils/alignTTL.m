40c3108d04c332f1296121dd1aabbef0fd62cc57
Xin <michaelniki1988@gmail.com>
2024-05-03 18:28:21 -0700

    add alignTTL

---
+++ ttlUtils/alignTTL.m
@@ -0,0 +1,56 @@
+function [ttlLogAlign, ttlCodeAlign, tsAlign] = alignTTL(ttlLog, ttlCode, ts)
+% ttlLog: cell array with columns: ts, ttl message, ttl code.
+% ttlCode: index ranging from 1 to 100
+% this function match ttlLog and ttlCode based on ttlcode and timestamps.
+% if there are multiple ttlLog files, concantenate them vertically.
+
+% TTL is a signal that marks the events during the experiment.
+% It is sent from the Mac/PC to the recording device.
+% If the experiment pauses on the Mac/PC, A new TTL log is created.
+% So we have multiple TTL logs on the Mac/PC (ttlLog), 
+% but only one on the recording machine (ttlCode)
+% testing TTLs are also not saved on ttlCode at the beginning. But it can
+% be save if a new experiment start. So far we only remove the testing TTLs
+% at the beginning of the first TTLlog.
+
+
+ttlLogCode = cell2mat(ttlLog(:, 3));
+ttlLogIdx = [];
+ttlCodeIdx = [];
+helper(1, 1);
+
+ttlLogAlign = ttlLog(ttlLogIdx, :);
+ttlCodeAlign = ttlCode(ttlCodeIdx);
+tsAlign = ts(ttlCodeIdx);
+
+
+    function helper(logStartIdx, codeStartIdx)
+        RemainingLength = min(length(ttlLogCode) - logStartIdx, length(ttlCode) - codeStartIdx) + 1;
+        
+        if RemainingLength <= 0
+            return
+        end
+
+        firstMatchIdx = find(ttlLogCode(logStartIdx:end) == ttlCode(codeStartIdx), 1, 'first');
+        firstMatchIdx = firstMatchIdx + logStartIdx - 1;
+
+        RemainingLength = RemainingLength - firstMatchIdx;
+        firstMismatchIdx = find(ttlLogCode(firstMatchIdx: firstMatchIdx + RemainingLength - 1) ~= ttlCode(codeStartIdx: codeStartIdx + RemainingLength - 1), 1, "first");
+
+        if firstMismatchIdx > 1
+            ttlLogIdx = [ttlLogIdx, firstMatchIdx: firstMatchIdx + firstMismatchIdx - 1];
+            ttlCodeIdx = [ttlCodeIdx, codeStartIdx: codeStartIdx + firstMismatchIdx - 1];
+        elseif isempty(firstMismatchIdx)
+            ttlLogIdx = [ttlLogIdx, firstMatchIdx: firstMatchIdx + RemainingLength - 1];
+            ttlCodeIdx = [ttlCodeIdx, codeStartIdx: codeStartIdx + RemainingLength - 1];
+            return
+        end
+
+        helper(firstMatchIdx + firstMismatchIdx, codeStartIdx + firstMismatchIdx)
+    end
+
+end
+
+
+
+
---
+++ ttlUtils/longestCommonSubsequence.m
@@ -0,0 +1,44 @@
+function [idx1, idx2, L] = LongestCommonSubsequence(arr1, arr2)
+
+    % This function finds the longest common subsequence that respects the frequencies
+    % of element occurrences up to each point in the arrays.
+    n = length(arr1);
+    m = length(arr2);
+    
+    % Dynamic programming table initialization
+    % L(i, j) will hold the length of the LCS of arr1(1:i) and arr2(1:j)
+    L = zeros(1, m+1);
+    
+    % We will also track the actual subsequences in a cell array and the
+    % index that constructs the subsequence for arr1 and arr2:
+    idx1 = cell(1, m+1);
+    idx2 = cell(1, m+1);
+    
+    % Build the LCS table
+    for i = 1:n
+        for j = 1:m
+            if arr1(i) == arr2(j)
+                % If elements match, extend the best previous LCS
+                L(1, j+1) = L(1, j) + 1;
+                idx1{1, j+1} = [idx1{1, j}, i];
+                idx2{1, j+1} = [idx2{1, j}, j];
+            else
+                % Otherwise, take the best of the two possible extensions without this element
+                if L(1, j) > L(1, j+1)
+                    L(1, j+1) = L(1, j);
+                    idx1{1, j+1} = idx1{1, j};
+                    idx2{1, j+1} = idx2{1, j};
+                else
+                    L(1, j+1) = L(1, j+1);
+                    idx1{1, j+1} = idx1{1, j+1};
+                    idx2{1, j+1} = idx2{1, j+1};
+                end
+            end
+        end
+    end
+    
+    % The longest common subsequence with respect to frequency
+    idx1 = idx1{end, end};
+    idx2 = idx2{end, end};
+    
+end
