d1c9af40b804f204e5f75e22d3ff4306df7dad96
Xin <michaelniki1988@gmail.com>
2024-05-06 15:44:04 -0700

    avoid reading all signals before spike detection

--- spikeDetection.m
+++ spikeDetection.m
@@ -12,13 +12,8 @@ if nargin < 5
     skipExist = false;
 end
 
-
 makeOutputPath(cscFiles, outputPath, skipExist)
-
 nSegments = length(timestampFiles);
-for j = nSegments:-1:1
-    [timestamps{j}, duration(j)] = readTimestamps(timestampFiles{j});
-end
 
 parfor i = 1: size(cscFiles, 1)
     channelFiles = cscFiles(i,:);
@@ -32,46 +27,27 @@ parfor i = 1: size(cscFiles, 1)
         continue
     end
 
-    param = set_parameters();
-    maxAmp = 500;
-
-    signals = cell(nSegments, 1);
     %outputStruct = cell(nSegments, 1);
     spikes = cell(nSegments, 1);
     spikeCodes = cell(nSegments, 1);
     spikeHist = cell(nSegments, 1);
     spikeHistPrecise = cell(nSegments, 1);
     spikeTimestamps = cell(nSegments, 1);
-    thr = zeros(nSegments, 1);
     xfDetect = cell(nSegments, 1);
-    outputStruct = cell(nSegments, 1);
 
-    for j = nSegments:-1:1
-        [signals{j}, samplingInterval] = readCSC(channelFiles{j});
-
-        param.sr = 1/samplingInterval;
-        param.ref = floor(1.5 * param.sr/1000);
-        [thr(j), outputStruct{j}] = getDetectionThresh(signals{j}, param, maxAmp);
-    end
-
-    outputStruct = cell2mat(outputStruct);
-
-    thr_all = min(thr);
-    % it shouldn't go less than 18. If it does, it probably found a file with a long stretch of flat, and will then find millions of spikes in the non-flat section.
-    thr_all = max(thr_all, 18);
-    maxThr = max([outputStruct.thrmax]);
-    common_noise_std_detect = min([outputStruct.noise_std_detect]);
-    common_noise_std_sorted = min([outputStruct.noise_std_sorted]);
+    [thr_all, outputStruct, param, maxAmp] = getDetectionThresh(channelFiles);
 
     for j = 1: nSegments
-        outputStruct(j).thrmax = maxThr;
-        outputStruct(j).noise_std_detect = common_noise_std_detect;
-        outputStruct(j).noise_std_sorted = common_noise_std_sorted;
-        outputStruct(j).thr = thr_all;
+        if isempty(channelFiles{j}, "file")
+            continue
+        end
 
-        [spikes{j}, thr, index, outputStruct(j), xfDetect{j}] = amp_detect_AS(signals{j}, param, maxAmp, timestamps{j}, thr_all, outputStruct(j));
-        spikeTimestamps{j} = timestamps{j}(index);
-        [spikeCodes{j}, spikeHist{j}, spikeHistPrecise{j}] = getSpikeCodes(spikes{j}, spikeTimestamps{j}, duration(j), param, outputStruct(j));
+        signal = readCSC(channelFiles{j});
+        [timestamps, duration] = readTimestamps(timestampFiles{j});
+
+        [spikes{j}, thr, index, outputStruct(j), xfDetect{j}] = amp_detect_AS(signal, param, maxAmp, timestamps, thr_all, outputStruct(j));
+        spikeTimestamps{j} = timestamps(index);
+        [spikeCodes{j}, spikeHist{j}, spikeHistPrecise{j}] = getSpikeCodes(spikes{j}, spikeTimestamps{j}, duration, param, outputStruct(j));
         if ~isempty(spikeCodes{j})
             spikeCodes{j}.ExpName = repmat(experimentName(j), height(spikeCodes{j}), 1);
         end
