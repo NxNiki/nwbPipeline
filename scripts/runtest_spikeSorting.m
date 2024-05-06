3ef8b120f5115fd0aa526fad1624efb9b099f52d
Xin <michaelniki1988@gmail.com>
2024-05-06 14:40:51 -0700

    [milestone] run pipeline for patient 573

--- scripts/runtest_spikeSorting.m
+++ scripts/runtest_spikeSorting.m
@@ -3,32 +3,31 @@ clear
 
 addpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/nwbPipeline'));
 
-expIds = (3: 11);
-filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/573_MovieParadigm';
+% expId = 2;
+% filePath = '/Users/XinNiuAdmin/Documents/NWBTest/output/Screening/572_Screening';
+
+% expId = 5;
+% filePath = '/Users/XinNiuAdmin/Documents/NWBTest/output/MovieParadigm/570_MovieParadigm';
+
+expId = 4;
+filePath = '/Users/XinNiuAdmin/Documents/NWBTest/output/MovieParadigm/570_MovieParadigm';
 
 
 % 0: will remove all previous unpack files.
 % 1: skip existing files.
-skipExist = 0;
+skipExist = 0; 
 
-microFiles = [];
-timestampFiles = [];
-for i = 1:length(expIds)
-    expFilePath = [filePath, '/Experiment', sprintf('%d/', expIds(i))];
-    microFilePath = fullfile(expFilePath, 'CSC_micro');
-    microFiles = [microFiles, readcell(fullfile(microFilePath, 'outFileNames.csv'), Delimiter=",")];
-    timestampFile = dir(fullfile(microFilePath, 'lfpTimeStamps*.mat'));
-    timestampFiles = [timestampFiles, fullfile(microFilePath, {timestampFile.name})];
-end
-
+expFilePath = [filePath, sprintf('/Experiment%d/', expId)];
 
 %% spike detection:
-delete(gcp('nocreate'))
-parpool(5);
 
-expFilePath = [filePath, '/Experiment', sprintf('-%d', expIds)];
-
+microFilePath = fullfile(expFilePath, 'CSC_micro');
 outputPath = fullfile(expFilePath, 'CSC_micro_spikes');
+
+microFiles = readcell(fullfile(microFilePath, 'outFileNames.csv'), Delimiter=",");
+% microFiles = microFiles(2,:);
+timestampFiles = dir(fullfile(microFilePath, 'lfpTimeStamps*.mat'));
+timestampFiles = fullfile(microFilePath, {timestampFiles.name});
 
 spikeDetection(microFiles, timestampFiles, outputPath, [], skipExist)
 disp('Spike Detection Finished!')
