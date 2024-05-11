056da115830d5c313aa6cfe7095c1c2381a34bf0
Xin Niu <michaelniki1988@gmail.com>
2024-05-10 19:32:55 -0700

    add batch extractLFP

---
+++ batch/runbatch_extractLFP.sh
@@ -0,0 +1,45 @@
+### matlab_test_job.sh START ###
+#!/bin/bash
+#$ -cwd
+# error = Merged with joblog
+#$ -o ../../cluster-output-spikeSort/job-$JOB_ID/joblog_$JOB_ID_$TASK_ID.txt
+#$ -j y
+## Edit the line below as needed:
+#$ -l h_rt=24:00:00,h_data=200G
+## Modify the parallel environment
+## and the number of cores as needed:
+#$ -pe shared 1
+# Email address to notify
+#$ -M $USER@mail
+# Notify when
+# #$ -m bea
+#$ -t 1:10:1  # 1-indexed here
+
+
+# echo job info on joblog:
+echo "Job $JOB_ID started on:   " `hostname -s`
+echo "Job $JOB_ID started on:   " `date `
+echo " "
+
+# load the job environment:
+. /u/local/Modules/default/init/modules.sh
+# To see which versions of matlab are available use: module av matlab
+module load matlab/R2023b
+echo "loaded matlab"
+
+total_tasks=$(( ($SGE_TASK_LAST - $SGE_TASK_FIRST) / $SGE_TASK_STEPSIZE + 1 ))
+
+echo "Start Matlab"
+matlab  -nosplash -nodisplay -singleCompThread -r "disp('getting started'), cd('/u/home/x/xinniu/nwbPipeline/batch/'), runbatch_extractLFP($SGE_TASK_ID, $total_tasks), disp('end'), exit"
+
+# echo job info on joblog:
+echo "Job $JOB_ID ended on:   " `hostname -s`
+echo "Job $JOB_ID ended on:   " `date `
+echo " "
+
+### extract_clusterless_parallel.job STOP ###
+# this site shows how to do array jobs: https://info.hpc.sussex.ac.uk/hpc-guide/how-to/array.html
+# (better than the Hoffman site https://www.hoffman2.idre.ucla.edu/Using-H2/Computing/Computing.html#how-to-build-a-submission-script)
+
+
+ 
