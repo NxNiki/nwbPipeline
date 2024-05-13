608a692acfbf5bca55605da410f783b9fe0990cc
Xin Niu <michaelniki1988@gmail.com>
2024-05-13 10:04:57 -0700

    add batch LFP

--- batch/runbatch_extractLFP.m
+++ batch/runbatch_extractLFP.m
@@ -1,41 +1,59 @@
-% run_extractLFP
-clear
+function runbatch_extractLFP(workerId, totalWorkers)
 
-expIds = (3:11);
-% filePath = '/Users/XinNiuAdmin/Documents/NWBTest/output/MovieParadigm/573_MovieParadigm';
+    if nargin < 1
+        % run script without queue:
+        workerId = 1;
+        totalWorkers = 1;
+    end
 
-filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/573_MovieParadigm';
+    addpath(genpath('/u/home/x/xinniu/nwbPipeline'));
 
-% 0: will remove all previous unpack files.
-% 1: skip existing files.
-skipExist = 0;
-saveRaw = false;
+    expIds = (3:11);
+    filePath = '/u/project/ifried/data/PIPELINE_vc/ANALYSIS/MovieParadigm/573_MovieParadigm';
 
-spikeFilePath = [filePath, '/Experiment', sprintf('-%d', expIds)];
-microLFPPath = fullfile(spikeFilePath, 'LFP_micro');
+    % 0: will remove all previous unpack files.
+    % 1: skip existing files.
+    skipExist = 1;
+    saveRaw = false;
 
-%% micro electrodes:
-microFiles = [];
-timestampFiles = [];
-for i = 1: length(expIds)
-    expId = expIds(i);
-    cscFilePath = [filePath, sprintf('/Experiment%d', expId)];
-    microFilePath = fullfile(cscFilePath, 'CSC_micro');
-    microFiles = [microFiles, readcell(fullfile(microFilePath, 'outFileNames.csv'), Delimiter=",")];
+    spikeFilePath = [filePath, '/Experiment', sprintf('-%d', expIds)];
+    microLFPPath = fullfile(spikeFilePath, 'LFP_micro');
 
-    timestampFilesExp = dir(fullfile(microFilePath, 'lfpTimeStamps*.mat'));
-    timestampFiles = [timestampFiles, fullfile(microFilePath, {timestampFilesExp.name})];
+    %% micro electrodes:
+    microFiles = [];
+    timestampFiles = [];
+    for i = 1: length(expIds)
+        expId = expIds(i);
+        cscFilePath = [filePath, sprintf('/Experiment%d', expId)];
+        microFilePath = fullfile(cscFilePath, 'CSC_micro');
+        microFiles = [microFiles, readcell(fullfile(microFilePath, 'outFileNames.csv'), Delimiter=",")];
+
+        timestampFilesExp = dir(fullfile(microFilePath, 'lfpTimeStamps*.mat'));
+        timestampFiles = [timestampFiles, fullfile(microFilePath, {timestampFilesExp.name})];
+    end
+
+    jobIds = splitJobs(size(microFiles, 1), totalWorkers, workerId);
+
+    if isempty(jobIds)
+        disp("No job assigned to batch! This is due to more workers than number of micro files.")
+        return
+    end
+
+    disp(['jobIds: ', sprintf('%d ', jobIds)]);
+    microFiles = microFiles(jobIds, :);
+    fprintf(['microFiles: \n', sprintf('%s\n', microFiles{:})]);
+
+    % delete(gcp('nocreate'))
+    % parpool(3); % each channel will take nearly 20GB memory for multi-exp analysis.
+
+    spikeFilePath = fullfile(spikeFilePath, 'CSC_micro_spikes');
+    [~, spikeFiles] = createSpikeFileName(microFiles(:, 1));
+    spikeFiles = cellfun(@(x) fullfile(spikeFilePath, x), spikeFiles, UniformOutput=false);
+
+    lfpFiles = extractLFP(microFiles, timestampFiles, spikeFiles, microLFPPath, '', skipExist, saveRaw);
+    writecell(lfpFiles, fullfile(microLFPPath, 'lfpFiles.csv'));
+
 end
-
-% delete(gcp('nocreate'))
-% parpool(3); % each channel will take nearly 20GB memory for multi-exp analysis.
-
-spikeFilePath = fullfile(spikeFilePath, 'CSC_micro_spikes');
-[~, spikeFiles] = createSpikeFileName(microFiles(:, 1));
-spikeFiles = cellfun(@(x) fullfile(spikeFilePath, x), spikeFiles, UniformOutput=false);
-
-lfpFiles = extractLFP(microFiles, timestampFiles, spikeFiles, microLFPPath, '', skipExist, saveRaw);
-writecell(lfpFiles, fullfile(microLFPPath, 'lfpFiles.csv'));
 
 
 
