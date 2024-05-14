627b4e722c55fda0c969be6d1787f7b61b022f3d
Xin Niu <michaelniki1988@gmail.com>
2024-05-14 14:48:51 -0700

    cd in shell intead of in matlab

--- batch/runbatch_extractLFP.sh
+++ batch/runbatch_extractLFP.sh
@@ -31,7 +31,8 @@ total_tasks=$(( ($SGE_TASK_LAST - $SGE_TASK_FIRST) / $SGE_TASK_STEPSIZE + 1 ))
 
 echo "Start Matlab"
 echo "run extractLFP, task id: $SGE_TASK_ID, total tasks: $total_tasks"
-matlab  -nosplash -nodisplay -singleCompThread -r "cd('/u/home/x/xinniu/nwbPipeline/batch/'), runbatch_extractLFP($SGE_TASK_ID, $total_tasks), exit"
+cd /u/home/x/xinniu/nwbPipeline/batch/
+matlab  -nosplash -nodisplay -singleCompThread -r "runbatch_extractLFP($SGE_TASK_ID, $total_tasks), exit"
 
 # echo job info on joblog:
 echo "Job $JOB_ID ended on:   " `hostname -s`
