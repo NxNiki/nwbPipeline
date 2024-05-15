b1dda168a3a8e65c7e63751d8216e9f314157e2a
Xin Niu <michaelniki1988@gmail.com>
2024-05-15 16:40:07 -0700

    increate h_data to avoid error in queued job

--- batch/runbatch_extractLFP.sh
+++ batch/runbatch_extractLFP.sh
@@ -5,7 +5,7 @@
 #$ -o ../../cluster-output-extractLFP/job-$JOB_ID/joblog_$JOB_ID_$TASK_ID.txt
 #$ -j y
 ## Edit the line below as needed:
-#$ -l h_rt=5:00:00,h_data=100G
+#$ -l h_rt=5:00:00,h_data=500G
 ## Modify the parallel environment
 ## and the number of cores as needed:
 #$ -pe shared 1
