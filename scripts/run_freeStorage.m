89de09b6fb63907ee383f64a40aac66785f912aa
Xin <michaelniki1988@gmail.com>
2024-05-17 19:21:15 -0700

    add parallel to backup files

--- scripts/run_freeStorage.m
+++ scripts/run_freeStorage.m
@@ -5,60 +5,16 @@ addpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/nwbPipeline'));
 
 expIds = (3:11);
 filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/573_MovieParadigm';
-
 backupPath = '/Volumes/DATA/HoffmanBackup/MovieParadigm/570_MovieParadigm';
+removeSource = true;
+
+%% backup files
 
 [microFiles, timestampFiles] = readFilePath(expIds, filePath);
 
-microFilesBackup = cellfun(@(x)strrep(x, filePath, backupPath), microFiles, UniformOutput=false);
-timestampFilesBackup = cellfun(@(x)strrep(x, filePath, backupPath), timestampFiles, UniformOutput=false);
+backupFiles(microFiles, backupPath, removeSource);
+backupFiles(timestampFiles, backupPath, removeSource);
 
-cellfun(@(x, y) moveFile(x, y), microFiles, microFilesBackup);
-cellfun(@(x, y) moveFile(x, y), timestampFiles, timestampFilesBackup);
-
-
-function moveFile(source, dest)
-    % Extract the directory part from the destination path
-    [destDir, ~, ~] = fileparts(dest);
-
-    % Check if the directory exists, and create it if it does not
-    if ~exist(destDir, 'dir')
-        mkdir(destDir);
-    end
-
-    % also copy IO record so that we can rerun analysis directly from LTS:
-    if ~exist(fullfile(destDir, 'outFileNames.csv'), "file")
-        [sourceDir, ~, ~] = fileparts(source);
-        IOLog = fullfile(sourceDir, 'outFileNames.csv');
-        if exist(IOLog, "file")
-            copyfile(IOLog, destDir);
-        end
-    end
-
-    % Move the file
-    if exist(source, "file")
-        fprintf('move file: %s \nto: %s\n', source, dest);
-
-        % Copy the file to the destination
-        copyStatus = copyfile(source, dest, 'f');
-        
-        if copyStatus
-            % Get information about the source and destination files
-            sourceInfo = dir(source);
-            destInfo = dir(dest);
-    
-            % Check if the destination file exists and is not corrupted
-            if exist(dest, 'file') && sourceInfo.bytes == destInfo.bytes
-                delete(source);
-            else
-                warning('dest file corrpted, try to backup again!');
-                delete(dest);
-            end
-        else
-            warning('File copy operation failed.');
-        end
-    end
-end
 
 
 
