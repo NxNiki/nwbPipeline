d0d842878dc08585ba6d2f6ab5b17c8616566c35
Xin Niu <michaelniki1988@gmail.com>
2024-05-22 14:16:39 -0700

    minor fix

--- scripts/run_spikeSorting.m
+++ scripts/run_spikeSorting.m
@@ -1,10 +1,12 @@
 % run spike detection and spike sorting to the unpacked data:
 clear
 
-addpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/nwbPipeline'));
+% addpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/nwbPipeline'));
+addpath(genpath('/u/home/x/xinniu/nwbPipeline'));
 
 expIds = (4:7);
-filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm';
+% filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm';
+filePath = '/u/project/ifried/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm';
 
 
 % 0: will remove all previous unpack files.
@@ -15,22 +17,15 @@ skipExist = 1;
 
 %% spike detection:
 delete(gcp('nocreate'))
-parpool(2); % each channel will take nearly 30GB memory for multi-exp analysis.
+parpool(1); % each channel will take nearly 30GB memory for multi-exp analysis.
 
 expFilePath = [filePath, '/Experiment', sprintf('-%d', expIds)];
-
 outputPath = fullfile(expFilePath, 'CSC_micro_spikes');
 
-spikeDetection(microFiles, timestampFiles, outputPath, expNames, skipExist)
+spikeFiles = spikeDetection(microFiles, timestampFiles, outputPath, expNames, skipExist);
 disp('Spike Detection Finished!')
 
 %% spike clustering:
-
-spikeFilePath = fullfile(expFilePath, 'CSC_micro_spikes');
-outputPath = fullfile(expFilePath, 'CSC_micro_spikes');
-
-spikeFiles = dir(fullfile(spikeFilePath, "*_spikes.mat"));
-spikeFiles = cellfun(@(f, n)fullfile(f, n), {spikeFiles.folder}, {spikeFiles.name}, UniformOutput=false);
 
 spikeClustering(spikeFiles, outputPath, skipExist);
 disp('Spike Clustering finished!')
