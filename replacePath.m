0b3088e252177e6279bed275f31ee8f98b33318f
Xin <michaelniki1988@gmail.com>
2024-05-23 13:49:43 -0700

    bug fix on clustering

--- replacePath.m
+++ replacePath.m
@@ -1,4 +1,8 @@
 function fullPath = replacePath(fullPath, replacePath)
+% IO files are generated with full path, which are different across platform.
+% This is used to change path when IO files are generated across platform
+% (i.e. on mac and hoffman).
+
     [~, fileName, ext] = fileparts(fullPath);
     fullPath = [replacePath, filesep, fileName, ext];
 end
