863776a7bfeed77f9e67e946c7ec1d6df56c6c54
Xin Niu <michaelniki1988@gmail.com>
2024-05-24 15:59:56 -0700

    fix file path running on hoffman

--- getDirectory.m
+++ getDirectory.m
@@ -1,16 +1,13 @@
-function [codeDir, workingDir] = getDirectory()
+function workingDir = getDirectory()
 
     if ismac
         % Code to run on Mac platform
         userHomeDir = getenv('HOME');
         workingDir = fullfile(userHomeDir, 'HoffmanMount/data/PIPELINE_vc/ANALYSIS');
-        codeDir = fullfile(userHomeDir, 'Documents/MATLAB/nwbPipeline');
     elseif isunix
         % Code to run on Linux platform
         userHomeDir = getenv('HOME');
         workingDir = '/u/project/ifried/data/PIPELINE_vc/ANALYSIS/';
-        codeDir = fullfile(userHomeDir, 'nwbPipeline');
-        
     else
         error(['getDirecotry only supports Mac and Linux platforms.\n' ...
             'You need to set working directory in the script or batch job']);
