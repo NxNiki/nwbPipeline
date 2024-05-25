dea00802a09a8ac27301e091718c6dbecb6f632f
Xin Niu <michaelniki1988@gmail.com>
2024-05-24 17:43:20 -0700

    fix bug in job script file

--- batch/runbatch_spikeSorting.sh
+++ batch/runbatch_spikeSorting.sh
@@ -36,7 +36,10 @@ cd /u/home/x/xinniu/nwbPipeline/batch
 
 # make a copy of batch script so that we can submit jobs for other patients
 # when previous jobs are still running:
-cp runbatch_spikeSorting.m runbatch_spikeSorting_$JOB_ID.m
+
+if [ ! -f "$file" ]; then
+    cp runbatch_spikeSorting.m runbatch_spikeSorting_$JOB_ID.m
+fi
 
 matlab  -nosplash -nodisplay -singleCompThread <<EOF
     runbatch_spikeSorting_$JOB_ID($SGE_TASK_ID, $total_tasks);
