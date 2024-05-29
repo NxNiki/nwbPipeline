242b93578de75c515cb49668ebcee2b8e940e09a
Xin <michaelniki1988@gmail.com>
2024-05-28 18:38:45 -0700

    restore backup files

--- scripts/run_freeStorage.m
+++ scripts/run_freeStorage.m
@@ -5,15 +5,20 @@ addpath(genpath('/Users/XinNiuAdmin/Documents/MATLAB/nwbPipeline'));
 
 expIds = (3:11);
 filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/573_MovieParadigm';
-backupPath = '/Volumes/DATA/HoffmanBackup/MovieParadigm/570_MovieParadigm';
+backupPath = '/Volumes/DATA/HoffmanBackup/MovieParadigm/573_MovieParadigm';
 removeSource = true;
 
-%% backup files
+%% backup files:
 
 [microFiles, timestampFiles] = readFilePath(expIds, filePath);
 
 backupFiles(microFiles, backupPath, removeSource);
 backupFiles(timestampFiles, backupPath, removeSource);
+
+
+%% restore backup files:
+
+copyfile(backupPath, filePath);
 
 
 
