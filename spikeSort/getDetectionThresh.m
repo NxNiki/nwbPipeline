5f2315d09c13a277f1bbaa7c463c422e37b5e64f
Xin <michaelniki1988@gmail.com>
2024-05-30 18:06:03 -0700

    refactor high pass filter in detection thresh and amp_detect_AS

--- spikeSort/getDetectionThresh.m
+++ spikeSort/getDetectionThresh.m
@@ -1,11 +1,10 @@
-function [outputStruct, param, maxAmp] = getDetectionThresh(channelFiles)
+function [outputStruct, param] = getDetectionThresh(channelFiles)
 % Does the first few steps of spike detection to find the threshold. This
 % allows for using the same threshold across multiple sessions. Returns
 % thr, as well as a struct with other variables created so that these steps
 % can be skipped in spikeDetect if the struct is passed in.
 
     param = set_parameters();
-    maxAmp = 500;
     thr = nan(length(channelFiles), 1);
     noise_std_detect = nan(length(channelFiles), 1);
     noise_std_sorted = nan(length(channelFiles), 1);
@@ -13,33 +12,10 @@ function [outputStruct, param, maxAmp] = getDetectionThresh(channelFiles)
 
     for i = 1:length(channelFiles)
         [x, samplingInterval] = readCSC(channelFiles{i});
+        % assume same sampling interval across channels.
         param.sr = 1/samplingInterval;
-        param.ref = floor(1.5 * param.sr/1000);
 
-        sr = param.sr;
-        stdmin = param.stdmin;
-        stdmax = param.stdmax;
-        fmin_detect = param.detect_fmin;
-        fmax_detect = param.detect_fmax;
-        fmin_sort = param.sort_fmin;
-        fmax_sort = param.sort_fmax;
-
-        %HIGH-PASS FILTER OF THE DATA
-        if exist('ellip','file')                  % Checks for the signal processing toolbox
-            [b_detect,a_detect] = ellip(param.detect_order, 0.1, 40, [fmin_detect fmax_detect]*2/sr);
-            [b,a] = ellip(param.sort_order, 0.1, 40, [fmin_sort fmax_sort]*2/sr);
-            xf_detect = filtfilt(b_detect, a_detect, x);
-            xf = filtfilt(b, a, x);
-        else
-            xf = fix_filter(x);                   % bandpass filtering between [300 3000] without the toolbox.
-            xf_detect = xf;
-        end
-
-        noise_std_detect(i) = median(abs(xf_detect))/0.6745;
-        noise_std_sorted(i) = median(abs(xf))/0.6745;
-        thr(i) = stdmin * noise_std_detect(i);        %thr for detection is based on detect settings.
-        thrmax(i) = min(maxAmp, stdmax * noise_std_sorted(i));     %thrmax for artifact removal is based on sorted settings.
-
+        [~, ~, noise_std_detect(i), noise_std_sorted(i), thr(i), thrmax(i)] = highPassSignal(x, param);
     end
 
     outputStruct.thrmax = max(thrmax);
