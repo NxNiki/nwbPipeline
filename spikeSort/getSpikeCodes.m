11e09372621e98e2fbfccee8f5cfbe13ea5e14c7
Xin <michaelniki1988@gmail.com>
2024-06-06 15:08:47 -0700

    reorganize getSpikeCode and better skip strategy

--- spikeSort/getSpikeCodes.m
+++ spikeSort/getSpikeCodes.m
@@ -1,29 +1,45 @@
 function outputFiles = getSpikeCodes(spikeFiles, outputPath, skipExist)
-    % SpikeCodes are features used to do clustering analysis. 
-    % Most of the spike codes can be calculated for each channel separately.
-    % `franctionConcurrent` and `frantionConcurrentPrecise` need information
-    % across all channels.
+% SpikeCodes are features used to do clustering analysis. 
+% Most of the spike codes can be calculated for each channel separately.
+% `franctionConcurrent` and `frantionConcurrentPrecise` need information
+% across all channels.
+% Note: will not skipExist if more channels are added as this will change
+% across channel spikeCodes (fractionConcurrent and
+% fractionConcurrentPrecise). 
+
+updateCrossChannelSpikeCode = false;
 
 makeOutputPath(spikeFiles, outputPath, skipExist)
-
 hasSpikes = cell(length(spikeFiles), 1);
 hasSpikesPrecise = cell(length(spikeFiles), 1);
-tempOutFileNames = cell(length(spikeFiles), 1);
+outFileNames = cell(length(spikeFiles), 2);
 
 % calculate spikeCodes:
 for fnum = 1:length(spikeFiles)
 
     spikeFile = spikeFiles{fnum};
     [~, filename, ext] = fileparts(spikeFile);
-    outFile = [strrep(filename, '_spikes', '_spikeCodesTemp'), ext];
 
-    tmpOutFile = ['temp_', outFile];
-    tempOutFileNames{fnum} = outFile;
+    outFileNames{fnum, 1} = fullfile(outputPath, [strrep(filename, '_spikes', '_spikeCodes1'), ext]);
+    outFileNames{fnum, 2} = fullfile(outputPath, [strrep(filename, '_spikes', '_spikeCodes'), ext]);
 
-    outFile = fullfile(outputPath, outFile);
-    tmpOutFile = fullfile(outputPath, tmpOutFile);
+    if exist(outFileNames{fnum, 2}, 'file') && skipExist
+        matobj = matfile(outFileNames{fnum, 2}, 'Writable', false);
+        hasSpikes{fnum} = matobj.spikeHist;
+        hasSpikesPrecise{fnum} = matobj.spikeHistPrecise;
+        continue
+    elseif exist(outFileNames{fnum, 1}, 'file') && skipExist
+        matobj = matfile(outFileNames{fnum, 1}, 'Writable', false);
+        hasSpikes{fnum} = matobj.spikeHist;
+        hasSpikesPrecise{fnum} = matobj.spikeHistPrecise;
+        continue
+    else
+        % new channels found, will recalculate all across channel
+        % spikeCodes. 
+        updateCrossChannelSpikeCode = true; 
+    end
 
-    fprintf('get spike codes:\n %s\n', outFile);
+    fprintf('get spike codes:\n %s\n', outFileNames{fnum, 1});
     spikeFileObj = matfile(spikeFile, 'Writable', false);
     param = spikeFileObj.param;
     outputStruct = spikeFileObj.outputStruct;
@@ -35,8 +51,11 @@ for fnum = 1:length(spikeFiles)
     spikeCodes = computeSpikeCodes(spikes, spikeTimestamps, param, outputStruct);
     [spikeHist, spikeHistPrecise] = calculateSpikeHist(spikeTimestamps, duration, param.sr);
 
-    fprintf('write spike codes to file:\n %s\n', outFile);
+    tmpOutFile = strrep(outFileNames{fnum, 1}, '_spikeCodes1', '_spikeCodes1Temp');
+    fprintf('write spike codes to file:\n %s\n', tmpOutFile);
     if exist(tmpOutFile, "file")
+        % delete unfinished temp file created in previous jobs. Otherwise
+        % writing to existing mat file will increase file size.
         delete(tmpOutFile);
     end
 
@@ -44,35 +63,28 @@ for fnum = 1:length(spikeFiles)
     matobj.spikeHist = spikeHist(:);
     matobj.spikeHistPrecise = spikeHistPrecise(:);
     matobj.spikeCodes = spikeCodes;
-    movefile(tmpOutFile, outFile);
+    movefile(tmpOutFile, outFileNames{fnum, 1});
 
     hasSpikes{fnum} = spikeHist(:);
     hasSpikesPrecise{fnum} = spikeHistPrecise(:);
+
 end
 
-outputFiles = cell(length(spikeFiles), 1);
+outputFiles = outFileNames(:, 2);
 hasSpikes = [hasSpikes{:}];
 hasSpikesPrecise = [hasSpikesPrecise{:}];
 percentConcurrentSpikes = sum(hasSpikes, 2)/length(spikeFiles);
 percentConcurrentSpikesPrecise = sum(hasSpikesPrecise, 2);
 
 % get across channel spike codes:
-for fnum = 1:length(tempOutFileNames)
+for fnum = 1:length(outFileNames)
     
-    spikeCodeFile = tempOutFileNames{fnum};
-    outFile = strrep(spikeCodeFile, '_spikeCodesTemp', '_spikeCodes');
-
-    tempOutFile = fullfile(outputPath, ['temp_', outFile]);
-    outFile = fullfile(outputPath, outFile);
-    outputFiles{fnum} = outFile;
-    
-    if exist(outFile, 'file') && skipExist
-        delete(fullfile(outputPath, spikeCodeFile));
+    if exist(outFileNames{fnum, 2}, 'file') && skipExist && ~updateCrossChannelSpikeCode
         continue
     end
 
-    fprintf('get cross channel spike codes:\n %s\n', outFile);
-    spikeCodeFileObj = matfile(fullfile(outputPath, spikeCodeFile), 'Writable', false);
+    fprintf('get cross channel spike codes:\n %s\n', outFileNames{fnum, 2});
+    spikeCodeFileObj = matfile(outFileNames{fnum, 1}, 'Writable', false);
     spikeCodes = spikeCodeFileObj.spikeCodes;
 
     spikeFileObj = matfile(spikeFiles{fnum}, 'Writable', false);
@@ -91,18 +103,19 @@ for fnum = 1:length(tempOutFileNames)
     fractionConcurrentPrecise = percentConcurrentSpikesPrecise(binPrecise);
     spikeCodes = [spikeCodes, table(fractionConcurrentPrecise(:), 'VariableNames', {'fractionConcurrentPrecise'})];
 
+    tempOutFile = strrep(outFileNames{fnum, 2}, '_spikeCodes', '_spikeCodesTemp');
     if exist(tempOutFile, 'file')
         delete(tempOutFile);
     end
-
-    fprintf('write spike codes to file:\n %s\n', outFile);
+    
+    fprintf('write spike codes to file:\n %s\n', tempOutFile);
     matobj = matfile(tempOutFile, 'Writable', true);
     matobj.spikeCodes = spikeCodes;
     matobj.spikeHist = spikeCodeFileObj.spikeHist;
     matobj.spikeHistPrecise = spikeCodeFileObj.spikeHistPrecise;
 
-    movefile(tempOutFile, outFile);
-    delete(fullfile(outputPath, spikeCodeFile));
-
+    movefile(tempOutFile, outFileNames{fnum, 2});
+    delete(outFileNames{fnum, 1});
+    
 end
 
