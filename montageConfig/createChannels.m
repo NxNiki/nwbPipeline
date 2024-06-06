5278644cbdf9d882cfccdb72711441c6e05e6b7f
Xin <michaelniki1988@gmail.com>
2024-06-06 12:23:48 -0700

    format code

--- montageConfig/createChannels.m
+++ montageConfig/createChannels.m
@@ -1,11 +1,11 @@
 function [macroChannels, microChannels] = createChannels(montageConfigFile)
 % read montage json file and save macro and micro channel names as cell 
-% arrays.
+% arrays. For macro Channels, unused channels will be filled with empty
+% value so that we can match to the .ncs files (unused channels will also
+% save files).
 
 montageConfig = readJson(montageConfigFile);
-
 macroChannels = {};
-
 [~, Data] = loadMacroChannels(montageConfig.macroChannels, montageConfig.miscChannels);
 for i = 1: size(Data, 1)
     numChannels = Data{i, 3} - Data{i, 2} + 1;
@@ -14,12 +14,10 @@ for i = 1: size(Data, 1)
         macroChannels(startIdx+j-1) = {[Data{i, 1}, num2str(j)]};
     end
 end
-
 macroChannels = macroChannels(:);
 
 microChannels = {};
 microConfig = montageConfig.Headstages;
-
 headStages = fieldnames(microConfig);
 for i = 1: length(headStages)
     ports = fieldnames(microConfig.(headStages{i}));
