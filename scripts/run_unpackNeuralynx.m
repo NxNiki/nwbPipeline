036761d2991dfb26113308c19f592bff2467ceb9
Xin <michaelniki1988@gmail.com>
2024-06-07 15:09:07 -0700

    add parallel config to unpack script add path on MontageConfigUI

--- scripts/run_unpackNeuralynx.m
+++ scripts/run_unpackNeuralynx.m
@@ -8,7 +8,8 @@ clear
 scriptDir = fileparts(mfilename('fullpath'));
 addpath(genpath(fileparts(scriptDir)));
 
-% expIds = (3: 11);
+%%% ----------- UCLA Data: --------- %%%
+% expIds = [3: 11];
 % filePath = {...
 %     '/Volumes/DATA/NLData/D573/EXP3_PreSleep_Movie24_Control_1_Home_Improvement/2024-05-03_20-54-34',...
 %     '/Volumes/DATA/NLData/D573/EXP4_PreSleep_Movie24_Control_2_Top_Gun/2024-05-03_21-30-59', ...
@@ -23,8 +24,20 @@ addpath(genpath(fileparts(scriptDir)));
 % 
 % outFilePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/573_MovieParadigm';
 
-% for testing:
-% expIds = (4:5);
+%%% -------- Iowa Data: ----------- %%%
+% expIds = [43, 44, 45, 46, 47, 50];
+% filePath = {...
+%     '/Volumes/DATA/NLData/i728R/728-043_UCLA Control Videos/2023-12-11_16-02-08', ...
+%     '/Volumes/DATA/NLData/i728R/728-044_UCLA Control Video Walking Dead/2023-12-11_16-22-45', ...
+%     '/Volumes/DATA/NLData/i728R/728-045_UCLA Movie 24/2023-12-11_17-06-47', ...
+%     '/Volumes/DATA/NLData/i728R/728-046_UCLA Movie 24 Recall/2023-12-11_18-48-32', ...
+%     '/Volumes/DATA/NLData/i728R/728-047_EEG_WatchPat_Overnight/2023-12-11_19-31-40', ...
+%     '/Volumes/DATA/NLData/i728R/728-050_Movie 24 Recall/2023-12-12_15-03-49', ...
+%     };
+% outFilePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/1728_MovieParadigm';
+
+%%% -------- for testing: --------- %%%
+% expIds = [4:5];
 % filePath = {...
 %     '/Users/XinNiuAdmin/HoffmanMount/xinniu/xin_test/RAW_NLX/D570/EXP4_Movie_24_Pre_Sleep/2024-01-26_20-46-57', ...
 %     '/Users/XinNiuAdmin/HoffmanMount/xinniu/xin_test/RAW_NLX/D570/EXP5_Movie_24_Sleep/2024-01-27_00-01-35', ...
@@ -32,34 +45,39 @@ addpath(genpath(fileparts(scriptDir)));
 % 
 % outFilePath = '/Users/XinNiuAdmin/HoffmanMount/xinniu/xin_test/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm';
 
-expIds = [43, 44, 45, 46, 47, 50];
-filePath = {...
-    '/Volumes/DATA/NLData/i728R/728-043_UCLA Control Videos/2023-12-11_16-02-08', ...
-    '/Volumes/DATA/NLData/i728R/728-044_UCLA Control Video Walking Dead/2023-12-11_16-22-45', ...
-    '/Volumes/DATA/NLData/i728R/728-045_UCLA Movie 24/2023-12-11_17-06-47', ...
-    '/Volumes/DATA/NLData/i728R/728-046_UCLA Movie 24 Recall/2023-12-11_18-48-32', ...
-    '/Volumes/DATA/NLData/i728R/728-047_EEG_WatchPat_Overnight/2023-12-11_19-31-40', ...
-    '/Volumes/DATA/NLData/i728R/728-050_Movie 24 Recall/2023-12-12_15-03-49', ...
-    };
-outFilePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/1728_MovieParadigm';
-
-
 skipExist = 1;
-% macroPattern = '^[RL].*[0-9]';
-% microPattern = '^G[A-D].*[0-9]';
-macroPattern = '^LFPx*';
-microPattern = '^PDes*';
+macroPattern = '^[RL].*[0-9]';
+microPattern = '^G[A-D].*[0-9]';
 
+% macroPattern = '^LFPx*';
+% microPattern = '^PDes*';
+
+%%% read montage setting to rename output file names
+% this is used on IOWA data on which .ncs files are named differently.
+% montageConfigFile = '/Users/XinNiuAdmin/Documents/MATLAB/nwbPipeline/montageConfig/montage_Patient-1728_exp-43.json';
+montageConfigFile = [];
+
+%%% define number of task in parfor:
+% generally we won't have memory issue in unpacking unless the raw ncs
+% files are combined for sleep experiments. 
+numParallelTasks = 4;
+% numParallelTasks = [];
+
+%%
 if ~exist("filePath", "var") || isempty(filePath)
     [filePath, expIds, outFilePath] = folderSelectionUI();
 end
 
-%% read montage setting to rename output file names
-% this is used on IOWA data on which .ncs files are named differently.
+if ~isempty('montageConfigFile')
+    [renameMacroChannels, renameMicroChannels] = createChannels(montageConfigFile);
+else
+    [renameMacroChannels, renameMicroChannels] = deal([]);
+end
 
-montageConfigFile = '/Users/XinNiuAdmin/Documents/MATLAB/nwbPipeline/montageConfig/montage_Patient-1728_exp-43.json';
-[renameMacroChannels, renameMicroChannels] = createChannels(montageConfigFile);
-
+if ~isempty(numParallelTasks)
+    delete(gcp('nocreate'))
+    parpool(numParallelTasks);
+end
 
 for i = 1:length(expIds)
 
