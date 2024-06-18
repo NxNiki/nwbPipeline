29d558d6160107fc46409dd153f1ebe6cebe4ef7
Xin Niu <michaelniki1988@gmail.com>
2024-06-18 16:00:09 -0700

    skip calculate xf if it is same as xf_detect

--- spikeSort/highPassFilter.m
+++ spikeSort/highPassFilter.m
@@ -12,9 +12,13 @@ function [xf_detect, xf, noise_std_detect, noise_std_sorted, thr, thrmax] = high
     %HIGH-PASS FILTER OF THE DATA
     if exist('ellip', 'file')                               % Checks for the signal processing toolbox
         [b_detect,a_detect] = ellip(param.detect_order, 0.1, 40, [fmin_detect fmax_detect]*2/sr);
-        [b,a] = ellip(param.sort_order, 0.1, 40, [fmin_sort fmax_sort]*2/sr);
         xf_detect = filtfilt(b_detect, a_detect, x);
-        xf = filtfilt(b, a, x);
+        if fmin_sort ~= fmin_detect || fmax_sort ~= fmax_detect || param.sort_order ~= param.detect_order
+            [b,a] = ellip(param.sort_order, 0.1, 40, [fmin_sort fmax_sort]*2/sr);
+            xf = filtfilt(b, a, x);
+        else
+            xf = xf_detect;
+        end
     else
         warning('ellip:notFound', 'ellip not found, use fix_filter to filter signal.');
         xf = fix_filter(x);                                 % bandpass filtering between [300 3000] without the toolbox.
