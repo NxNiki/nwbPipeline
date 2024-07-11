6723bd31d89243923bebaf16d23877d0b504a282
Xin <michaelniki1988@gmail.com>
2024-07-10 17:38:27 -0700

    bug fix on wave_clus

--- scripts/run_extractLFP.m
+++ scripts/run_extractLFP.m
@@ -1,31 +1,38 @@
 % run_extractLFP
 clear
 
-expIds = (3:11);
-% filePath = '/Users/XinNiuAdmin/Documents/NWBTest/output/MovieParadigm/573_MovieParadigm';
-
-filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/573_MovieParadigm';
+expIds = (4);
+filePath = '/Users/XinNiuAdmin/Documents/NWBTest/output/MovieParadigm/570_MovieParadigm';
+% expIds = (3:11);
+% filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/573_MovieParadigm';
 
 % 0: will remove all previous unpack files.
 % 1: skip existing files.
 skipExist = 1;
 saveRaw = false;
 
-spikeFilePath = [filePath, '/Experiment', sprintf('-%d', expIds)];
-microLFPPath = fullfile(spikeFilePath, 'LFP_micro');
-
+expFilePath = [filePath, '/Experiment', sprintf('-%d', expIds)];
 %% micro electrodes:
+microLFPPath = fullfile(expFilePath, 'LFP_micro');
 [microFiles, timestampFiles] = readFilePath(expIds, filePath);
 
 % delete(gcp('nocreate')) 
 % parpool(3); % each channel will take nearly 20GB memory for multi-exp analysis.
 
-spikeFilePath = fullfile(spikeFilePath, 'CSC_micro_spikes');
+spikeFilePath = fullfile(expFilePath, 'CSC_micro_spikes');
 [~, spikeFiles] = createSpikeFileName(microFiles(:, 1));
 spikeFiles = cellfun(@(x) fullfile(spikeFilePath, x), spikeFiles, UniformOutput=false);
 
-lfpFiles = extractLFP(microFiles, timestampFiles, spikeFiles, microLFPPath, '', skipExist, saveRaw);
+lfpFiles = extractLFP(microFiles, timestampFiles, spikeFiles, '', microLFPPath, '', skipExist, saveRaw);
 writecell(lfpFiles, fullfile(microLFPPath, 'lfpFiles.csv'));
 
+%% macro electrodes:
+macroLFPPath = fullfile(expFilePath, 'LFP_macro');
+[macroFiles, timestampFiles] = readFilePath(expIds, filePath, 'macro');
 
+% delete(gcp('nocreate')) 
+% parpool(3); % each channel will take nearly 20GB memory for multi-exp analysis.
+
+lfpFiles = extractLFP(macroFiles, timestampFiles, '', '', macroLFPPath, '', skipExist, saveRaw);
+writecell(lfpFiles, fullfile(macroLFPPath, 'lfpFiles.csv'));
 
