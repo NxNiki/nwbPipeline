54e3fc2021619b0cb1a2ae2a9928b1f1da8191ab
Xin <michaelniki1988@gmail.com>
2024-07-11 17:13:46 -0700

    refactor and bug fix on rejectedSpikeIdx not in times file

--- spikeSort/wave_clus/readData_ASCIISpikePreClustered.m
+++ spikeSort/wave_clus/readData_ASCIISpikePreClustered.m
@@ -5,7 +5,7 @@ function [cluster_class, tree, clu, handles] = readData_ASCIISpikePreClustered(f
 % handles.par = set_parameters_ascii_spikes(filename,handles);     %Load parameters
 handles.par = set_joint_parameters_CSC(filename); %EM: Replaced the default wave_clus parameters with those that we've been using
 % HISTOGRAM PARAMETERS
-for i=1:handles.par.max_clus+1
+for i=1:handles.par.max_clus + 1
     handles.par.(['nbins' num2str(i-1)]) = 100;  % # of bins for the ISI histograms
     handles.par.(['bin_step' num2str(i-1)]) = 1;  % percentage number of bins to plot
 end
@@ -32,18 +32,20 @@ spikeFileObj = matfile(fullfile(pathname, filename));
 % load the times_ file. So we just remove _spikes from the filename
 % first.
 filename = strrep(filename,'_spikes','');
-timesFile = fullfile(pathname, ['times_',filename]);
-if ~exist(timesFile,'file')
+timesFile = fullfile(pathname, ['times_', filename]);
+if ~exist(timesFile, 'file')
     warning([timesFile, ' does not exist. Move on...'])
     return
 end
-load(timesFile, 'cluster_class', 'spikeIdxRejected', 'inspk');
-index=cluster_class(:,2)'; %timestamps of spikes; gets loaded in line above.
+
+timesFileObj = matfile(timesFile);
+cluster_class = timesFileObj.cluster_class;
+spikeTimestamps=cluster_class(:,2)'; %timestamps of spikes; gets loaded in line above.
 
 %Load clustering results
 fname = [handles.par.fname '_' filename(1:end-4)];               % filename for interaction with SPC
 if ~exist([fname '.dg_01.lab'], 'file')
-    fname = strrep(fname,'CSC', 'ch');
+    fname = strrep(fname, 'CSC', 'ch');
 end
 clu  = load(fullfile(pathname, [fname '.dg_01.lab']));
 tree = load(fullfile(pathname, [fname '.dg_01']));
@@ -53,7 +55,7 @@ handles.par.fnamesave = fname;
 USER_DATA = get(handles.wave_clus_figure, 'userdata');
 
 if exist('ipermut', 'var')
-    clu_aux = zeros(size(clu,1),length(index)) + 1000;
+    clu_aux = zeros(size(clu,1), length(spikeTimestamps)) + 1000;
     for i=1:length(ipermut)
         clu_aux(:,ipermut(i)+2) = clu(:,i+2);
     end
@@ -62,12 +64,18 @@ if exist('ipermut', 'var')
     USER_DATA{12} = ipermut;
 end
 
-spikes = spikeFileObj.spikes;
-spikes(spikeIdxRejected, :) = [];
+if ismember("spikeIdxRejected", who(timesFileObj))
+    spikes = spikeFileObj.spikes;
+    spikes(timesFileObj.spikeIdxRejected, :) = [];
+else
+    spikes = timesFileObj.spikes;
+end
+
 USER_DATA{2} = spikes;
-USER_DATA{3} = index(:)';
+USER_DATA{3} = spikeTimestamps(:)';
 USER_DATA{4} = clu;
 USER_DATA{5} = tree;
-USER_DATA{7} = inspk;
-set(handles.wave_clus_figure, 'userdata', USER_DATA) % I (Xin) really hate this way of passing data to wave_clus, we need to fix this.
+USER_DATA{7} = timesFileObj.inspk;
+set(handles.wave_clus_figure, 'userdata', USER_DATA)            % I (Xin) really hate this way of passing data to wave_clus, we need to fix this.
+
 end
