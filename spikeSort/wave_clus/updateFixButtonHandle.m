4a13c59bcbb41edd3f11bb64763729cba3a39891
Xin <michaelniki1988@gmail.com>
2024-07-12 19:16:59 -0700

    refactor and bug fix

---
+++ spikeSort/wave_clus/updateFixButtonHandle.m
@@ -0,0 +1,18 @@
+function updateFixButtonHandle(hObject, handles)
+
+USER_DATA = get(handles.wave_clus_figure,'userdata');
+par = USER_DATA{1};
+
+set(handles.fix1_button,'value',0);
+set(handles.fix2_button,'value',0);
+set(handles.fix3_button,'value',0);
+for i=4:par.max_clus
+    par.(['fix', num2str(i)]) = 0;
+end
+
+USER_DATA{1} = par;
+set(handles.wave_clus_figure, 'userdata', USER_DATA)
+
+guidata(hObject, handles);
+
+end
---
+++ spikeSort/wave_clus/updateParam.m
@@ -0,0 +1,11 @@
+function updateParam(hObject, handles, parKey)
+
+USER_DATA = get(handles.wave_clus_figure, 'userdata');
+par = USER_DATA{1};
+par.(parKey) = str2double(get(hObject, 'String'));
+USER_DATA{1} = par;
+set(handles.wave_clus_figure,'userdata', USER_DATA);
+
+guidata(hObject, handles);
+
+end
