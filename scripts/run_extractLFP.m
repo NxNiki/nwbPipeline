9cb06e5fac0936692e2536e37e8cd0bef1f6da8e
Xin <michaelniki1988@gmail.com>
2024-07-16 10:36:56 -0700

    export to nwb (wip)

--- scripts/run_extractLFP.m
+++ scripts/run_extractLFP.m
@@ -6,7 +6,7 @@ filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParad
 
 % 0: will remove all previous unpack files.
 % 1: skip existing files.
-skipExist = [1, 0];
+skipExist = [0, 1];
 saveRaw = false;
 
 expFilePath = [filePath, '/Experiment', sprintf('-%d', expIds)];
@@ -18,10 +18,11 @@ microLFPPath = fullfile(expFilePath, 'LFP_micro');
 % parpool(3); % each channel will take nearly 20GB memory for multi-exp analysis.
 
 spikeFilePath = fullfile(expFilePath, 'CSC_micro_spikes');
-[~, spikeFiles] = createSpikeFileName(microFiles(:, 1));
-spikeFiles = cellfun(@(x) fullfile(spikeFilePath, x), spikeFiles, UniformOutput=false);
+[spikeDetectFiles, spikeClusterFiles] = createSpikeFileName(microFiles(:, 1));
+spikeDetectFiles = cellfun(@(x) fullfile(spikeFilePath, x), spikeDetectFiles, UniformOutput=false);
+spikeClusterFiles = cellfun(@(x) fullfile(spikeFilePath, x), spikeClusterFiles, UniformOutput=false);
 
-lfpFiles = extractLFP(microFiles, timestampFiles, spikeFiles, '', microLFPPath, '', skipExist(1), saveRaw);
+lfpFiles = extractLFP(microFiles, timestampFiles, spikeDetectFiles, spikeClusterFiles, microLFPPath, '', skipExist(1), saveRaw);
 writecell(lfpFiles, fullfile(microLFPPath, 'lfpFiles.csv'));
 
 %% macro electrodes:
