807d504151d96c94f9a455aba53d4d18e5957810
Xin <michaelniki1988@gmail.com>
2024-07-16 16:52:52 -0700

    bug fix on handles

--- spikeSort/wave_clus/updateHandles.m
+++ spikeSort/wave_clus/updateHandles.m
@@ -1,4 +1,9 @@
-function updateHandles(hObject, handles, fieldsTrue, fieldsFalse, clusterIdx)
+function handles = updateHandles(hObject, handles, fieldsTrue, fieldsFalse, clusterIdx, minclus)
+% set handles to 1 for keys in fieldsTrue and 0 for keys in fieldsFalse.
+% if clusterIdx is set, load USER_DATA and get minclus. Otherwise set
+% minclus directly. If both clusterIdx and minclus are empty, minclus is
+% not set.
+
     if nargin < 3
         fieldsTrue = [];
     end
@@ -6,12 +11,19 @@ function updateHandles(hObject, handles, fieldsTrue, fieldsFalse, clusterIdx)
         fieldsFalse = [];
     end
     if nargin < 5
-        clusterIdx = 10;
+        clusterIdx = [];
     end
-    
-    USER_DATA = get(handles.wave_clus_figure, 'userdata');
-    cluster_results = USER_DATA{clusterIdx};
-    handles.minclus = cluster_results(1, 5);
+    if nargin < 6
+        minclus = [];
+    end
+
+    if ~isempty(clusterIdx)
+        USER_DATA = get(handles.wave_clus_figure, 'userdata');
+        cluster_results = USER_DATA{clusterIdx};
+        handles.minclus = cluster_results(1, 5);
+    elseif ~isempty(minclus)
+        handles.minclus = minclus;
+    end
 
     for i = 1:length(fieldsTrue)
         handles.(fieldsTrue{i}) = 1;
