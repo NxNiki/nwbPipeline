bc8d57300461a289a94b5c8ccf51bd3127484bfc
Xin <michaelniki1988@gmail.com>
2024-07-18 12:05:40 -0700

    bug fix on missing csc files in combineCSC

--- utils/readTimestamps.m
+++ utils/readTimestamps.m
@@ -1,4 +1,4 @@
-function [timestamps, duration] = readTimestamps(filename)
+function [timestamps, duration, samplingIntervalSeconds] = readTimestamps(filename)
 
 tsFileObj = matfile(filename, 'Writable', false);
 ts = tsFileObj.timeStamps;
@@ -8,6 +8,12 @@ if isa(tsFileObj.timeend, 'duration')
     duration = seconds(tsFileObj.timeend - tsFileObj.time0);
 else
     duration = tsFileObj.timeend - tsFileObj.time0;
+end
+
+if isa(tsFileObj.samplingInterval, "duration")
+    samplingIntervalSeconds = seconds(tsFileObj.samplingInterval);
+else
+    samplingIntervalSeconds = tsFileObj.samplingInterval;
 end
 
 end
