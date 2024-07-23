241280d2c7b74f82d305694a1f229d65c07faf0e
Xin <michaelniki1988@gmail.com>
2024-07-23 15:50:18 -0700

    unpack black rock, add test script

--- utils/unpackBlackRock.m
+++ utils/unpackBlackRock.m
@@ -1,19 +1,33 @@
 function unpackBlackRock(inFile, expFilePath, channelNames, skipExist)
-
 % Reads analog samples BETWEEN t0 and t1.
 % Actual time_stamps are returned in t
+
+
+if nargin < 3
+    channelNames = [];
+end
+
+if nargin < 4
+    skipExist = [1, 1];
+end
 
 if length(skipExist) == 1
     skipExist = [skipExist, skipExist];
 end
 
 tic
-[timestampsFile, electrodeInfoFile] = blackrock_read_header(inFile, expFilePath, skipExist(1));
-
+[~, electrodeInfoFile] = blackrock_read_header(inFile, expFilePath, [], skipExist(1));
 toc
 
 %% Read data in in chunks and split by channel
 
 tic
-blackrock_read_channel(inFile, expFilePath, timestampsFile, electrodeInfoFile, skipExist(2))
+tempOutFiles = blackrock_read_channel(inFile, expFilePath, electrodeInfoFile, skipExist(2));
+
+if length(channelNames) == length(tempOutFiles)
+    for i = 1: length(tempOutFiles)
+        path = fileparts(tempOutFiles{i});
+        movefile(tempOutFiles{i}, fullfile(path, channelNames{i}));
+    end
+end
 tic
