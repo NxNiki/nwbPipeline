a268d85dc754aa7a5167ca6a7a846efd0eaa22a0
Xin <michaelniki1988@gmail.com>
2024-07-22 17:53:50 -0700

    unpack blackrock (wip)

--- utils/unpackBlackRock.m
+++ utils/unpackBlackRock.m
@@ -1,4 +1,4 @@
-function unpackBlackRock(inFile, expFilePath, channelNames, skipExist)
+function outFiles = unpackBlackRock(inFile, expFilePath, channelNames, skipExist)
 % Reads analog samples BETWEEN t0 and t1.
 % Actual time_stamps are returned in t
 
@@ -15,19 +15,17 @@ if length(skipExist) == 1
     skipExist = [skipExist, skipExist];
 end
 
+%% read header and create timestamps file:
+
 tic
-[~, electrodeInfoFile] = blackrock_read_header(inFile, expFilePath, [], skipExist(1));
+[~, electrodeInfoFile] = blackrock_read_header(inFile, expFilePath, skipExist(1));
 toc
 
 %% Read data in in chunks and split by channel
 
 tic
-tempOutFiles = blackrock_read_channel(inFile, expFilePath, electrodeInfoFile, skipExist(2));
-
-if length(channelNames) == length(tempOutFiles)
-    for i = 1: length(tempOutFiles)
-        path = fileparts(tempOutFiles{i});
-        movefile(tempOutFiles{i}, fullfile(path, channelNames{i}));
-    end
-end
+outFiles = blackrock_read_channel(inFile, electrodeInfoFile, skipExist(2), channelNames);
 tic
+
+writetable(inFile, fullfile(fileparts(outFiles{1}), 'inFileNames.csv'));
+writetable(outFiles, fullfile(fileparts(outFiles{1}), 'outFileNames.csv'));
