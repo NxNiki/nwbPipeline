508262d6848d3f8a5e46a86c450c80b73068e146
Xin <michaelniki1988@gmail.com>
2024-07-30 18:18:03 -0700

    bug fix on black rock io

--- BlackRockIO/blackrock_read_channel.m
+++ BlackRockIO/blackrock_read_channel.m
@@ -13,26 +13,32 @@ outputFilePath = fileparts(electrodeInfoFile);
 electrodeInfoObj = matfile(electrodeInfoFile);
 NSx = electrodeInfoObj.NSx;
 channelId = NSx.MetaTags.ChannelID;
-channelId = channelId(channelId <= 128);
-nchan = length(channelId);
+channelIdx = channelId <= 256;
+channelId = channelId(channelIdx);
 
 % trailing null characters (ASCII code 0) are often used in C-style strings 
 % to indicate the end of the string but can be problematic in MATLAB.
-outFiles = cellfun(@(x)fullfile(outputFilePath, [x(double(x) ~= 0), '.mat']), {NSx.ElectrodesInfo(:).Label}, 'UniformOutput', false);
+outFiles = cellfun(@(x)fullfile(outputFilePath, [x(double(x) ~= 0), '.mat']), {NSx.ElectrodesInfo(channelIdx).Label}, 'UniformOutput', false);
+nchan = length(outFiles);
 
 parfor i = 1: nchan
     if skipExist && exist(outFiles{i}, 'file') 
         continue
     end
     fprintf('writing data to: %s\n', outFiles{i});
-    NSx = openNSx('report','read', inFile, 'channels', channelId(i), 'uV', 'precision', 'double');
+    NSx = openNSx('report','read', inFile, 'channels', i, 'uV', 'precision', 'double');
 
     data = NSx.Data;
     samplingInterval = seconds(1) / NSx.MetaTags.SamplingFreq;
 
-    outFileObj = matfile(outFiles{i});
+    tmpOutFile = strrep(outFiles{i}, '.mat', 'tmp.mat');
+    if exist(tmpOutFile, 'file')
+        delete(tmpOutFile)
+    end
+    outFileObj = matfile(tmpOutFile);
     outFileObj.data = data;
     outFileObj.samplingInterval = samplingInterval;
+    movefile(tmpOutFile, outFiles{i});
 end
 
 fprintf('Done.\n');
