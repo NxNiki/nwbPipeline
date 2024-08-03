a69c1bf82bcffab1f2eb4d18f2d7fc6494a1bbfc
Xin <michaelniki1988@gmail.com>
2024-08-02 18:24:00 -0700

    bug fix on filter

--- spikeSort/highPassFilter.m
+++ spikeSort/highPassFilter.m
@@ -8,11 +8,11 @@ function [xf_detect, xf, noise_std_detect, noise_std_sorted, thr, thrmax] = high
     fmax_detect = param.detect_fmax;
     fmin_sort = param.sort_fmin;
     fmax_sort = param.sort_fmax;
-    
+    x = double(x);
     %HIGH-PASS FILTER OF THE DATA
     if exist('ellip', 'file')                               % Checks for the signal processing toolbox
         [b_detect,a_detect] = ellip(param.detect_order, 0.1, 40, [fmin_detect fmax_detect]*2/sr);
-        xf_detect = filtfilt(b_detect, a_detect, double(x));
+        xf_detect = filtfilt(b_detect, a_detect, x);
         if fmin_sort ~= fmin_detect || fmax_sort ~= fmax_detect || param.sort_order ~= param.detect_order
             [b,a] = ellip(param.sort_order, 0.1, 40, [fmin_sort fmax_sort]*2/sr);
             xf = filtfilt(b, a, x);
