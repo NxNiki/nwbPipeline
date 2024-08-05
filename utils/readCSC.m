94c0a47defab6fe759d8c6f1ed2d73b9e1f7c1cb
Xin <michaelniki1988@gmail.com>
2024-08-05 16:02:04 -0700

    use int16 for unpacked blackrock data

--- utils/readCSC.m
+++ utils/readCSC.m
@@ -13,8 +13,10 @@ if runRemovePLI && ismember('signalRemovePLI', who('-file', filename))
     runRemovePLI = false;
 elseif ismember('ADBitVolts', who('-file', filename)) && ~isnan(matObj.ADBitVolts)
     signal = single(signal(:)) * matObj.ADBitVolts * 1e6; % convert signal to micro volt
+elseif ismember('BlackRockUnits', who('-file', filename))
+    signal = single(signal(:)) / matObj.BlackRockUnits; % black rock data is in 1/4 uV.
 else
-    message = 'ADBitVolts is NaN; your CSC data will not be scaled';
+    message = 'ADBitVolts or BlackRockUnits not exists; your CSC data will not be scaled';
     warning(message);
     % logMessage(logFile, message);
 end
