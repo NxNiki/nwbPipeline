e46b6388025fe8101b5268480d33d644eec51961
Xin <michaelniki1988@gmail.com>
2024-08-07 16:28:34 -0700

    unpack macro test data

--- test/test_unpackNeuralynx.m
+++ test/test_unpackNeuralynx.m
@@ -1,9 +1,11 @@
 % test unpack neuralynx data
+% use 
 
 
 scriptDir = fileparts(mfilename('fullpath'));
 addpath(genpath(fileparts(scriptDir)));
 
+%% unpack micro files:
 ncsFiles = {
     fullfile(scriptDir, 'neuralynx/raw/CSC_1.ncs')
     fullfile(scriptDir, 'neuralynx/raw/CSC_2.ncs')
@@ -16,7 +18,24 @@ outFiles = {
     'GD4-LEC1_001.mat'
     };
 
-
 outFiles = unpackData(ncsFiles, outFiles, fullfile(scriptDir, 'neuralynx/CSC_micro'), 1, 0);
 writecell(ncsFiles, fullfile(scriptDir, 'neuralynx/CSC_micro/inFileNames.csv'));
 writecell(outFiles, fullfile(scriptDir, 'neuralynx/CSC_micro/outFileNames.csv'));
+
+
+%% unpack macro files:
+ncsFiles = {
+    fullfile(scriptDir, 'neuralynx/raw/CSC_1.ncs')
+    fullfile(scriptDir, 'neuralynx/raw/CSC_2.ncs')
+    fullfile(scriptDir, 'neuralynx/raw/CSC_3.ncs')
+    };
+
+outFiles = {
+    'GA1-RA1_001.mat'
+    'GB2-RAH1_001.mat'
+    'GD4-LEC1_001.mat'
+    };
+
+outFiles = unpackData(ncsFiles, outFiles, fullfile(scriptDir, 'neuralynx/CSC_macro'), 1, 0);
+writecell(ncsFiles, fullfile(scriptDir, 'neuralynx/CSC_macro/inFileNames.csv'));
+writecell(outFiles, fullfile(scriptDir, 'neuralynx/CSC_macro/outFileNames.csv'));
