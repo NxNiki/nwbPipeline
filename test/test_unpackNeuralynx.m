d13da263c5e62db0ef60bd88e745cbf95627f3d8
Xin <michaelniki1988@gmail.com>
2024-08-07 17:02:18 -0700

    update test data and fix micro file name without suffix

--- test/test_unpackNeuralynx.m
+++ test/test_unpackNeuralynx.m
@@ -7,15 +7,17 @@ addpath(genpath(fileparts(scriptDir)));
 
 %% unpack micro files:
 ncsFiles = {
-    fullfile(scriptDir, 'neuralynx/raw/CSC_1.ncs')
-    fullfile(scriptDir, 'neuralynx/raw/CSC_2.ncs')
-    fullfile(scriptDir, 'neuralynx/raw/CSC_3.ncs')
+    fullfile(scriptDir, 'neuralynx/raw/GA1-RAH1.ncs')
+    fullfile(scriptDir, 'neuralynx/raw/GA2-RAC4.ncs')
+    fullfile(scriptDir, 'neuralynx/raw/GA4-ROF2.ncs')
+
     };
 
 outFiles = {
-    'GA1-RA1_001.mat'
-    'GB2-RAH1_001.mat'
-    'GD4-LEC1_001.mat'
+    'GA1-RAH1_001.mat' % ensure spike sort can handle suffix.
+    'GA2-RAC4.mat'
+    'GA4-ROF2.mat'
+
     };
 
 outFiles = unpackData(ncsFiles, outFiles, fullfile(scriptDir, 'neuralynx/CSC_micro'), 1, 0);
@@ -25,15 +27,15 @@ writecell(outFiles, fullfile(scriptDir, 'neuralynx/CSC_micro/outFileNames.csv'))
 
 %% unpack macro files:
 ncsFiles = {
-    fullfile(scriptDir, 'neuralynx/raw/CSC_1.ncs')
-    fullfile(scriptDir, 'neuralynx/raw/CSC_2.ncs')
-    fullfile(scriptDir, 'neuralynx/raw/CSC_3.ncs')
+    fullfile(scriptDir, 'neuralynx/raw/RAH1.ncs')
+    fullfile(scriptDir, 'neuralynx/raw/RAC4.ncs')
+    fullfile(scriptDir, 'neuralynx/raw/ROF2.ncs')
     };
 
 outFiles = {
-    'GA1-RA1_001.mat'
-    'GB2-RAH1_001.mat'
-    'GD4-LEC1_001.mat'
+    'RAH1.mat'
+    'RAC4.mat'
+    'ROF2.mat'
     };
 
 outFiles = unpackData(ncsFiles, outFiles, fullfile(scriptDir, 'neuralynx/CSC_macro'), 1, 0);
