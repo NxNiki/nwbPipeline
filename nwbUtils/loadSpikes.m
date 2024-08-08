6b1d1b9070efe33007f7714900bafd7c5015df97
Xin <michaelniki1988@gmail.com>
2024-08-07 18:36:04 -0700

    fix bug on missing times file nwb export

--- nwbUtils/loadSpikes.m
+++ nwbUtils/loadSpikes.m
@@ -1,10 +1,11 @@
-function [unitsTimeStamp, unitsWaveForm, unitsWaveFormMean, electrodeIndex] = loadSpikes(spikesFiles, timesFiles)
+function [unitsTimeStamp, unitsWaveForm, unitsWaveFormMean, electrodeIndex] = loadSpikes(spikeFilePath)
 % read units in spike files and concatenate timestamps, waveform, and file
 % name in a one dimensional cell array.
 % this is used to save spikes into nwb file.
 
+spikeFiles = listFiles(spikeFilePath, '*_spikes.mat', '^\._');
 
-numFiles = length(timesFiles);
+numFiles = length(spikeFiles);
 unitsTimeStamp = cell(numFiles, 1);
 unitsWaveForm = cell(numFiles, 1);
 unitsWaveFormMean = cell(numFiles, 1);
@@ -12,20 +13,32 @@ electrodeIndex = cell(numFiles, 1);
 
 parfor i = 1:numFiles
 
-    fprintf('loadSpikes: %s\n', timesFiles{i});
+    fprintf('loadSpikes: %s\n', spikeFiles{i});
 
-    timesFileObj = matfile(timesFiles{i});
-    spikeClass = timesFileObj.cluster_class(:, 1);
-    spikeTimestamps = timesFileObj.cluster_class(:, 2)';
+    [~, spikeFileName] = fileparts(spikeFiles{i});
+    timesFile = fullfile(spikeFilePath, ['times_', strrep(spikeFileName, '_spikes.mat', '.mat')])
 
-    spikesFilesObj = matfile(spikesFiles{i});
+    spikesFilesObj = matfile(spikeFiles{i});
     spikes = spikesFilesObj.spikes;
-    spikes(timesFileObj.spikeIdxRejected, :) = [];
-    
-    % remove un-clustered spikes:
-    spikes = spikes(spikeClass ~= 0,:);
-    spikeClass = spikeClass(spikeClass ~= 0);
-    
+
+    if exist(timesFile, "file")
+        
+        timesFileObj = matfile(timesFile);
+        spikeClass = timesFileObj.cluster_class(:, 1);
+        spikeTimestamps = timesFileObj.cluster_class(:, 2)';
+
+        spikes(timesFileObj.spikeIdxRejected, :) = [];
+        
+        % remove un-clustered spikes:
+        spikes = spikes(spikeClass ~= 0,:);
+        spikeClass = spikeClass(spikeClass ~= 0);
+
+    else
+        warning('times file not exist: %s\n', timesFile);
+        spikeClass = ones(size(spikes, 1), 1);
+        spikeTimestamps = spikesFilesObj.spikeTimestamps
+    end
+
     units = unique(spikeClass);
     unitsTimeStampi = cell(length(units), 1);
     unitsWaveFormi = cell(length(units), 1);
