71b6f2107ec1144cd793827456f16169005114ce
Xin <michaelniki1988@gmail.com>
2024-08-13 18:00:55 -0700

    [tweak] update message to user

--- spikeSort/wave_clus/readData_ASCIISpikePreClustered.m
+++ spikeSort/wave_clus/readData_ASCIISpikePreClustered.m
@@ -46,7 +46,7 @@ timesFileObj = matfile(timesFile);
 cluster_class = timesFileObj.cluster_class;
 manualTimesFile = fullfile(pathname, ['times_manual_' filename]);
 if exist(manualTimesFile, 'file')
-    message = 'This spike file has been manually sorted, do you want to load the previous result?';
+    message = 'This spike file has been manually sorted, do you want to load the manually sorted result?';
     title = 'Spikes manually sorted already!';
     option1 = 'Yes';
     option2 = 'No';
