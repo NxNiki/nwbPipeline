5af8d9f9ba13a562b0d24c6361681a681480efd1
Xin <michaelniki1988@gmail.com>
2024-08-26 11:07:41 -0700

    [fix] format output and fix rules to scale timestamps

--- tools/fix_times_file.m
+++ tools/fix_times_file.m
@@ -2,41 +2,57 @@
 % select cluster_class and sortBy and create a new times file.
 
 clear
-searth_path = '/Users/XinNiuAdmin/HoffmanMount/bfalken/MICROS';
-files = dir(fullfile(searth_path, '**/times_*.mat'));
-ExpDurationSeconds = 5.1315e+04;
+searth_path = '/Users/XinNiuAdmin/HoffmanMount/bfalken/MICROS/566 Experiment';
+files = dir(fullfile(searth_path, '**/times_G[A-D]*.mat'));
+ExpDurationSeconds = 2e+04;
+skipExists = false;
 
 for i = 1:length(files)
     path = files(i).folder;
+    fname = strrep(files(i).name, 'times_', 'times_manual_');
+    outFile = fullfile(path, fname);
+
+    if exist(outFile, "file") && skipExists
+        fprintf("file exist: %s\n", outFile);
+        continue
+    end
+
     load(fullfile(path, files(i).name), 'cluster_class');
 
     min_spike_time = min(cluster_class(:, 2));
     max_spike_time = max(cluster_class(:, 2));
+    exp_duration = get_experiment_duration(cluster_class);
 
     factor = 1;
 
     while 1
-        if min_spike_time / factor < .1
-            break
-        end
-
-        if (max_spike_time - min_spike_time) / factor > ExpDurationSeconds
+        if exp_duration / factor > ExpDurationSeconds * 100
             factor = factor * 1000;
-            disp('scale time stamps')
+            fprintf('scale time stamps with factor: %d\n', factor)
+        elseif exp_duration / factor < ExpDurationSeconds/100
+            factor = factor / 1000;
+            fprintf('scale time stamps with factor: %d\n', factor)
         else
             break
         end
     end
 
-    files(i).name
-    factor
     cluster_class(:, 2) = cluster_class(:, 2) / factor;
-    mean(diff(cluster_class(:, 2)))
-    max(cluster_class(:, 2)) - min(cluster_class(:, 2))
+    exp_duration = get_experiment_duration(cluster_class);
+
+    fprintf("file: %s\n", outFile);
+    fprintf("timestamps factor: %d\n", factor);
+    fprintf("mean diff on timestamps: %f\n", mean(diff(cluster_class(:, 2))));
+    fprintf("duration of experiment (sec): %f\n", exp_duration);
 
     save(fullfile(path, files(i).name), 'cluster_class', '-append');
 
-    fname = strrep(files(i).name, 'times_', 'times_manual_');
     sortBy = {'Ben', char(datetime("now"))};
-    save(fullfile(path, fname), 'cluster_class', 'sortBy');
+    save(outFile, 'cluster_class', 'sortBy');
+end
+
+function exp_duration = get_experiment_duration(cluster_class)
+    min_spike_time = min(cluster_class(:, 2));
+    max_spike_time = max(cluster_class(:, 2));
+    exp_duration = max_spike_time - min_spike_time;
 end
