29afab72b13b99f0d1eae178a5b14971f4439bf8
Xin <michaelniki1988@gmail.com>
2024-08-27 11:38:55 -0700

    add some comments to scripts

--- scripts/run_extractLFP.m
+++ scripts/run_extractLFP.m
@@ -7,6 +7,8 @@ filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/FaceRecogn
 % 0: will remove all previous extracted files.
 % 1: skip existing files.
 skipExist = [0, 1];
+
+% save raw data to check spike interpolation (will take huge storage space)
 saveRaw = false;
 
 expFilePath = [filePath, '/Experiment', sprintf('-%d', expIds)];
