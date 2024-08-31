2d53a3b6eb56daf15bf6700e08675f14374dceeb
Xin <michaelniki1988@gmail.com>
2024-08-30 18:13:55 -0700

    [style] minor updates

--- scripts/run_extractLFP.m
+++ scripts/run_extractLFP.m
@@ -1,12 +1,15 @@
 % run_extractLFP
 clear
 
-expIds = [4];
-filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/FaceRecognition/486_FaceRecognition';
+expIds = [3:4];
+patient = 573;
+expName = 'MovieParadigm';
+
+filePath = sprintf('/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/%s/%d_%s', expName, patient, expName);
 
 % 0: will remove all previous extracted files.
 % 1: skip existing files.
-skipExist = [0, 1];
+skipExist = [0, 1];  %[micro, macro]
 
 % save raw data to check spike interpolation (will take huge storage space)
 saveRaw = false;
