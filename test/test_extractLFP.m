47de0736d26380de146eec2846a6e4982f3cdaa2
Xin <michaelniki1988@gmail.com>
2024-09-03 12:08:25 -0700

    [style] minor change

--- test/test_extractLFP.m
+++ test/test_extractLFP.m
@@ -9,12 +9,14 @@ expFilePath = [scriptDir, '/neuralynx'];
 lfpFs = 2000;  %Hz
 % 0: will remove all previous unpack files.
 % 1: skip existing files.
-skipExist = [0, 0]; 
+skipExist = [0, 0];
 saveRaw = 0;
 
 microFilePath = fullfile(expFilePath, 'CSC_micro');
 macroFilePath = fullfile(expFilePath, 'CSC_macro');
+
 %% save down sampled timestamps for micro and macro channels:
+
 microLFPPath = fullfile(expFilePath, 'LFP_micro');
 macroLFPPath = fullfile(expFilePath, 'LFP_macro');
 
@@ -25,11 +27,12 @@ timestampFiles = dir(fullfile(microFilePath, 'lfpTimeStamps*.mat'));
 macroTimestampFiles = fullfile(microFilePath, {timestampFiles.name});
 
 lfpTimestamps = downsampleTimestamps(microTimestampFiles, macroTimestampFiles, lfpFs, expFilePath);
+
 %% micro electrodes:
 
 microFiles = readcell(fullfile(microFilePath, 'outFileNames.csv'), Delimiter=",");
-
 spikeFilePath = fullfile(expFilePath, 'CSC_micro_spikes');
+
 [spikeDetectFiles, spikeClusterFiles] = createSpikeFileName(microFiles(:, 1));
 spikeClusterFiles = cellfun(@(x) fullfile(spikeFilePath, x), spikeClusterFiles, UniformOutput=false);
 spikeDetectFiles = cellfun(@(x) fullfile(spikeFilePath, x), spikeDetectFiles, UniformOutput=false);
@@ -43,4 +46,3 @@ macroFiles = readcell(fullfile(macroFilePath, 'outFileNames.csv'), Delimiter=","
 
 lfpFiles = extractLFP(macroFiles, macroTimestampFiles, lfpTimestamps, '', '', macroLFPPath, skipExist(2), saveRaw);
 writecell(lfpFiles, fullfile(macroLFPPath, 'lfpFiles.csv'));
-
