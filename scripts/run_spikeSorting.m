315f5dea8c40277b61901d26725dfa98a9981be5
Xin <michaelniki1988@gmail.com>
2024-09-04 19:21:11 -0700

    [teak] minor updates

--- scripts/run_spikeSorting.m
+++ scripts/run_spikeSorting.m
@@ -5,14 +5,14 @@ clear
 scriptDir = fileparts(mfilename('fullpath'));
 addpath(genpath(fileparts(scriptDir)));
 
-expIds = [11,12,13];
-filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/557_MovieParadigm';
+expIds = [46, 47, 48];
+filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/1702_MovieParadigm';
 
 % 0: will remove all previous unpack files.
 % 1: skip existing files.
 skipExist = [0, 0, 0];
 
 % remove noises caused by power line interference:
-runRemovePLI = false;
+runRemovePLI = true;
 
 batch_spikeSorting(1, 1, expIds, filePath, skipExist, runRemovePLI);
