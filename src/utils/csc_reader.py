de025f966cce611df4a19f3f5dbc26d34606438d
Xin <michaelniki1988@gmail.com>
2024-09-16 11:31:21 -0700

    [style] fix mypy and pylint issue

--- src/utils/csc_reader.py
+++ src/utils/csc_reader.py
@@ -1,19 +1,21 @@
 import os.path
+import warnings
+from datetime import timedelta
+from typing import List, Optional, Tuple, Union
 
 import numpy as np
-import warnings
+import numpy.typing as npt
 from mat73 import loadmat
-from datetime import timedelta
-from typing import List, Union, Optional, Tuple
-
 
 GAP_THRESHOLD = 2
+
+
 def combine_csc(
-        timestamp_files: List[str],
-        signal_files: Optional[List[str]],
-        max_gap_duration: Optional[float]=None,
-        use_single_precision: bool=True
-) -> Tuple[np.ndarray, np.ndarray, float, float]:
+    timestamp_files: List[str],
+    signal_files: Optional[List[str]] = None,
+    max_gap_duration: Optional[float] = None,
+    use_single_precision: bool = True,
+) -> Tuple[npt.NDArray[np.float_], Optional[npt.NDArray[np.float_]], float, float]:
     """
     Combine CSC signals, filling gaps with NaNs if the gap between segments is larger than the threshold.
     Set signal_files to empty to only process timestamp_files.
@@ -29,89 +31,185 @@ def combine_csc(
         max_gap_duration = np.inf
 
     # Error checking for empty files and mismatch lengths
-    if len(timestamp_files) != len(signal_files) and signal_files:
-        raise ValueError("combine_CSC: mismatch in length of timestamp_files and signal_files")
+    if signal_files is not None and len(timestamp_files) != len(signal_files):
+        raise ValueError(
+            "combine_csc: mismatch in length of timestamp_files and signal_files"
+        )
 
-    process_signal = True
-    if not signal_files:
-        process_signal = False
-        warnings.warn("combine_csc: no signal files provided")
+    timestamps_to_combine, sampling_intervals, signal_to_combine = read_data(
+        timestamp_files, signal_files
+    )
+    check_data_length(timestamps_to_combine, signal_to_combine)
+    sampling_interval_seconds = check_sampling_intervals(sampling_intervals)
 
+    combined_timestamps, combined_signal = fill_gaps(
+        timestamps_to_combine,
+        signal_to_combine,
+        max_gap_duration,
+        sampling_interval_seconds,
+    )
+
+    combined_timestamps, timestamps_start = process_timestamps(
+        combined_timestamps, use_single_precision
+    )
+
+    if use_single_precision and combined_signal is not None:
+        combined_signal = combined_signal.astype(np.float32)
+
+    return (
+        combined_timestamps,
+        combined_signal,
+        sampling_interval_seconds,
+        timestamps_start,
+    )
+
+
+def read_data(
+    timestamp_files: List[str], signal_files: Optional[List[str]]
+) -> Tuple[
+    List[npt.NDArray[np.float_]],
+    npt.NDArray[np.float_],
+    Optional[List[npt.NDArray[np.float_]]],
+]:
     num_files = len(timestamp_files)
+    if signal_files is None or len(signal_files) == 0:
+        warnings.warn("combine_csc: no signal files provided")
+        signal_files = [""] * num_files
 
-    signal_combined = [None] * num_files
-    timestamps_combined = [None] * num_files
+    signal_to_combine = [np.array([])] * num_files
+    timestamps_to_combine = [np.array([])] * num_files
     sampling_intervals = np.full(num_files, np.nan)
 
     # Read timestamps and signals
     for i in range(num_files):
-        timestamps_combined[i], _, sampling_intervals[i] = read_timestamps(timestamp_files[i])
-        if process_signal:
-            if file_exists(signal_files[i]):
-                print(f"Reading CSC (order {i + 1}): {signal_files[i]}")
-                signal_combined[i], sampling_interval_csc = read_csc(signal_files[i])
-                if np.isnan(sampling_intervals[i]):
-                    sampling_intervals[i] = sampling_interval_csc
-            else:
-                warnings.warn(f"CSC file does not exist: {signal_files[i]}. Data will be filled with NaNs")
-                signal_combined[i] = np.full(len(timestamps_combined[i]), np.nan)
+        timestamps_to_combine[i], _, sampling_intervals[i] = read_timestamps(
+            timestamp_files[i]
+        )
+        if file_exists(signal_files[i]):
+            print(f"Reading CSC (order {i + 1}): {signal_files[i]}")
+            signal_to_combine[i], sampling_interval_csc = read_csc(signal_files[i])
+            if np.isnan(sampling_intervals[i]):
+                sampling_intervals[i] = sampling_interval_csc
+        else:
+            warnings.warn(
+                f"CSC file does not exist: {signal_files[i]}. Data will be filled with NaNs"
+            )
+            signal_to_combine[i] = np.full(len(timestamps_to_combine[i]), np.nan)
 
-    # Check lengths of signals and timestamps
-    timestamp_lengths = [len(t) for t in timestamps_combined]
-    signal_lengths = [len(s) if s is not None else 0 for s in signal_combined]
+    if signal_files is None or len(signal_files) == 0:
+        return timestamps_to_combine, sampling_intervals, signal_to_combine
 
-    if any(signal_lengths) and any(ts_len != sig_len for ts_len, sig_len in zip(timestamp_lengths, signal_lengths)):
-        raise ValueError("Mismatched length of signal and timestamps")
+    return timestamps_to_combine, sampling_intervals, None
 
+
+def check_sampling_intervals(sampling_intervals: npt.NDArray[np.float_]) -> float:
     unique_intervals = np.unique(sampling_intervals[~np.isnan(sampling_intervals)])
     if len(unique_intervals) > 1:
         raise ValueError("Sampling intervals do not match across files!")
+    return float(unique_intervals.item())
 
-    sampling_interval_duration = sampling_intervals[0]
 
-    # Fill gaps between experiments/segments with NaNs
-    signal_gap = [None] * num_files
-    timestamps_gap = [None] * num_files
+def process_timestamps(
+    timestamps: npt.NDArray[np.float_], use_single_precision: bool
+) -> Tuple[npt.NDArray[np.float_], float]:
+    """
+    convert timestamps form unix time to relative time with start time.
+    :param timestamps:
+    :param use_single_precision: if true, try to convert timestamps to single precision, start time will not be
+        converted.
+    :return:
+    """
+    timestamps_start = timestamps[0].item()
+    timestamps -= timestamps_start
+
+    if use_single_precision:
+        timestamps_single = timestamps.astype(np.float32)
+        if not np.all(np.isclose(np.diff(timestamps), 0)):
+            timestamps = timestamps_single
+
+    return timestamps, timestamps_start
+
+
+def check_data_length(
+    timestamps_to_combine: List[npt.NDArray[np.float_]],
+    signals_to_combine: Optional[List[npt.NDArray[np.float_]]],
+) -> None:
+    """
+    check if length of timestamps and signal match for each segment.
+    :param timestamps_to_combine:
+    :param signals_to_combine:
+    :return:
+    """
+
+    if signals_to_combine is None or len(signals_to_combine) == 0:
+        return
+
+    timestamp_lengths = [len(t) if t is not None else 0 for t in timestamps_to_combine]
+    signal_lengths = [len(s) if s is not None else 0 for s in signals_to_combine]
+
+    if any(signal_lengths) and any(
+        ts_len != sig_len for ts_len, sig_len in zip(timestamp_lengths, signal_lengths)
+    ):
+        raise ValueError("Mismatched length of signal and timestamps")
+
+
+def fill_gaps(
+    timestamps: List[npt.NDArray[np.float_]],
+    signals: Optional[List[npt.NDArray[np.float_]]],
+    max_gap_duration: float,
+    sampling_interval_seconds: float,
+) -> Tuple[npt.NDArray[np.float_], Optional[npt.NDArray[np.float_]]]:
+    """
+    Fill gaps between experiments/segments with NaNs.
+    :param timestamps:
+    :param signals:
+    :param max_gap_duration:
+    :param sampling_interval_seconds:
+    :return:
+    """
+
+    num_files = len(timestamps)
+    signal_gap = [np.array([])] * num_files
+    timestamps_gap = [np.array([])] * num_files
     for i in range(1, num_files):
-        gap_interval = min((timestamps_combined[i][0] - timestamps_combined[i - 1][-1]), max_gap_duration)
-        if gap_interval / sampling_interval_duration > GAP_THRESHOLD:
-            gap_length = int(np.floor(gap_interval / sampling_interval_duration))
+        gap_interval = min(
+            (timestamps[i][0].item() - timestamps[i - 1][-1].item()),
+            max_gap_duration,
+        )
+        if gap_interval / sampling_interval_seconds > GAP_THRESHOLD:
+            gap_length = int(np.floor(gap_interval / sampling_interval_seconds))
             signal_gap[i - 1] = np.full(gap_length, np.nan)
             timestamps_gap[i - 1] = np.arange(
-                timestamps_combined[i - 1][-1] + sampling_interval_duration,
-                timestamps_combined[i - 1][-1] + gap_length * sampling_interval_duration,
-                sampling_interval_duration
+                timestamps[i - 1][-1] + sampling_interval_seconds,
+                timestamps[i - 1][-1] + gap_length * sampling_interval_seconds,
+                sampling_interval_seconds,
             )
 
     # Combine timestamps and signals
-    timestamps_combined = [
-        np.hstack((timestamps_combined[i], timestamps_gap[i] if timestamps_gap[i] is not None else []))
-        for i in range(num_files)]
-    combined_timestamps = np.hstack(timestamps_combined)
+    timestamps_to_combine = [
+        np.hstack(
+            (
+                timestamps[i],
+                timestamps_gap[i] if timestamps_gap[i] is not None else [],
+            )
+        )
+        for i in range(num_files)
+    ]
+    combined_timestamps = np.hstack(timestamps_to_combine)
 
-    if process_signal:
-        signal_combined = [np.hstack((signal_combined[i], signal_gap[i] if signal_gap[i] is not None else []))
-                           for i in range(num_files)]
-        combined_signal = np.hstack(signal_combined)
+    if signals is not None:
+        signal_to_combine = [
+            np.hstack((signals[i], signal_gap[i] if signal_gap[i] is not None else []))
+            for i in range(num_files)
+        ]
+        combined_signal = np.hstack(signal_to_combine)
     else:
         combined_signal = None
 
-    if combined_timestamps.size == 0:
-        return combined_signal, [], sampling_interval_duration, []
+    return combined_timestamps, combined_signal
 
-    # Adjust timestamps for precision
-    timestamps_start = combined_timestamps[0]
-    combined_timestamps -= timestamps_start
 
-    if use_single_precision:
-        combined_timestamps = np.single(combined_timestamps)
-        if np.all(np.diff(combined_timestamps) != 0):
-            combined_timestamps = np.single(combined_timestamps)
-        combined_signal = np.single(combined_signal) if combined_signal is not None else None
-
-    return combined_signal, combined_timestamps, sampling_interval_duration, timestamps_start
-
-def read_timestamps(filename: str) -> Tuple[np.ndarray, np.ndarray, float]:
+def read_timestamps(filename: str) -> Tuple[npt.NDArray[np.float_], float, float]:
     """
     Read timestamps from a .mat file. Handles different versions where the samplingInterval may or may not exist
     and can be a duration object or a float (in seconds). The function will return the sampling interval as a float
@@ -124,19 +222,20 @@ def read_timestamps(filename: str) -> Tuple[np.ndarray, np.ndarray, float]:
     mat_data = loadmat(filename)
 
     # Load timestamps
-    timestamps = mat_data['timeStamps'].flatten()
+    timestamps = mat_data["timeStamps"].flatten()
 
     # Calculate duration
-    time_end = mat_data['timeendSeconds'].item()
-    time_start = mat_data['time0'].item()
+    time_end = mat_data["timeendSeconds"].item()
+    time_start = mat_data["time0"].item()
     duration = time_end - time_start
 
     # Check for the existence of samplingInterval and handle different formats
-    sampling_interval_seconds = mat_data['samplingIntervalSeconds'].item()
+    sampling_interval_seconds = mat_data["samplingIntervalSeconds"].item()
 
     return timestamps, duration, sampling_interval_seconds
 
-def read_csc(filename: str):
+
+def read_csc(filename: str) -> Tuple[npt.NDArray[np.float_], float]:
     """
     Read CSC signal from a .mat file and convert the samplingInterval to seconds.
 
@@ -148,18 +247,20 @@ def read_csc(filename: str):
     mat_data = loadmat(filename)
 
     # Read the signal
-    signal = mat_data['data'].flatten()
+    csc_signal = mat_data["data"].flatten()
 
     # Check if the file contains signalRemovePLI
-    if 'ADBitVolts' in mat_data and not np.isnan(mat_data['ADBitVolts']):
-        signal = signal.astype(np.float32) * mat_data['ADBitVolts'].item() * 1e6  # Convert to micro volts
+    if "ADBitVolts" in mat_data and not np.isnan(mat_data["ADBitVolts"]):
+        csc_signal = (
+            csc_signal.astype(np.float32) * mat_data["ADBitVolts"].item() * 1e6
+        )  # Convert to micro volts
     else:
         warning_message = "ADBitVolts is NaN; your CSC data will not be scaled."
         warnings.warn(warning_message)
 
     # Handle sampling interval
-    if 'samplingIntervalSeconds' in mat_data:
-        sampling_interval_seconds = mat_data['samplingIntervalSeconds'].item()
+    if "samplingIntervalSeconds" in mat_data:
+        sampling_interval_seconds = mat_data["samplingIntervalSeconds"].item()
         # Ensure the units of sampling intervals are appropriate
         while 1 / sampling_interval_seconds < 500:
             warnings.warn("Scaling samplingIntervalSeconds down by 1000")
@@ -167,7 +268,8 @@ def read_csc(filename: str):
     else:
         raise ValueError("samplingIntervalSeconds not found in the file")
 
-    return signal, sampling_interval_seconds
+    return csc_signal, sampling_interval_seconds
+
 
 def file_exists(filepath: str) -> bool:
     """
@@ -176,17 +278,23 @@ def file_exists(filepath: str) -> bool:
     return os.path.exists(filepath)
 
 
-
-if __name__ == '__main__':
-
-    csc_files = [
-        '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm/Experiment-5/CSC_macro/LA9_004.mat',
-        '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm/Experiment-5/CSC_macro/LA9_005.mat'
+if __name__ == "__main__":
+    csc_files_test = [
+        "/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm/Experiment-5/"
+        "CSC_macro/LA9_004.mat",
+        "/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm/Experiment-5/"
+        "CSC_macro/LA9_005.mat",
     ]
-    timestamp_files = [
-        '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm/Experiment-5/CSC_macro/lfpTimeStamps_004.mat',
-        '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm/Experiment-5/CSC_macro/lfpTimeStamps_005.mat'
+    timestamp_files_test = [
+        "/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm/Experiment-5/"
+        "CSC_macro/lfpTimeStamps_004.mat",
+        "/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/570_MovieParadigm/Experiment-5/"
+        "CSC_macro/lfpTimeStamps_005.mat",
     ]
 
-    combined_signal, combined_timestamps, sampling_interval_duration, timestamps_start = combine_csc(timestamp_files,
-                                                                                                     csc_files)
+    (
+        signal_test,
+        timestamps_test,
+        sampling_interval_duration_test,
+        timestamps_start_test,
+    ) = combine_csc(timestamp_files_test, csc_files_test)
