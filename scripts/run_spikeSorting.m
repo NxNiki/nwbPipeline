c4e59a23ea19f816ade1477ca00b60fba8f972e0
Xin <michaelniki1988@gmail.com>
2024-09-25 11:51:08 -0700

    [tweak] minor update

--- scripts/run_spikeSorting.m
+++ scripts/run_spikeSorting.m
@@ -5,16 +5,21 @@ clear
 scriptDir = fileparts(mfilename('fullpath'));
 addpath(genpath(fileparts(scriptDir)));
 
-expIds = [5];
-filePath = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/Screening/576_Screening';
+expIds = [48:51];
+expName = 'MovieParadigm';
+patient_id = 1741;
+
+
+filePath = sprintf('/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/%s/%d_%s', expName, patient_id, expName);
+
 
 % 0: will remove all previous unpack files.
 % 1: skip existing files.
-skipExist = [0, 0, 0];
+skipExist = [1, 1, 0]; % [spike detection, spike code, spike clustering]
 
 % remove noises caused by power line interference:
 runRemovePLI = true;
 
-maxNumCompThreads(10);
+maxNumCompThreads(4);
 
 batch_spikeSorting(1, 1, expIds, filePath, skipExist, runRemovePLI);
