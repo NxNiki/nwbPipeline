bb10342bee67a15f56f57e70130fdb2b5fe4e842
Xin <michaelniki1988@gmail.com>
2024-09-25 11:44:39 -0700

    [fix] bug fix in check field name of par

--- src/spikeSort/check_WC_params.m
+++ src/spikeSort/check_WC_params.m
@@ -2,10 +2,10 @@ function check_WC_params(par)
 
 if isfield(par,'features') && strcmp('features','wav')
 
-    if isfield(par,'scales')&&  isfield(par,'par.w_post') &&  isfield(par,'par.w_pre')
+    if isfield(par, 'scales') &&  isfield(par, 'w_post') &&  isfield(par, 'w_pre')
          L = wmaxlev(par.w_pre+par.w_post, 'haar');
-         if L<scale
-             error('[par.scale] exceeds maximum wavelet decomposition level for a waveform of length [par.w_pre+par.w_post]')
+         if L < par.scales
+             error('[par.scales] exceeds maximum wavelet decomposition level for a waveform of length [par.w_pre+par.w_post]')
          end
     end
     if isfield(par,'par.w_post') &&  isfield(par, 'par.w_pre')
