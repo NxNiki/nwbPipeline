f0d591d0ab0ce8383399e42f8650718c55be9311
Xin <michaelniki1988@gmail.com>
2024-10-28 15:11:03 -0700

    [refactor] isolate unpack events

---
+++ src/NlxIO/unpackEvents.m
@@ -0,0 +1,45 @@
+function outFileNames = unpackEvents(inFileNames, outFileNames, outFilePath, verbose, skipExist)
+if nargin < 4 || isempty(verbose)
+    verbose = 1;
+end
+
+if nargin < 5
+    skipExist = 1;
+end
+
+makeOutputPath(inFileNames, outFilePath, skipExist)
+
+for i = 1:length(inFileNames)
+
+    if skipExist && exist(outFileName, "file") && checkMatFileCorruption(outFileName) 
+        continue
+    end
+
+    if exist(outFileNameTemp, "file")
+        warning('delete temp file: %s\n', outFileNameTemp);
+        delete(outFileNameTemp);
+    end
+
+    if verbose
+        fprintf('unpack event file: %s\nto: %s\n', inFileNames{i}, outFileName);
+    end
+
+    [~, outFileName, ~] = fileparts(outFileNames{i});
+    outFileNameTemp = fullfile(outFilePath, [outFileName, 'temp.mat']);
+    outFileName = fullfile(outFilePath, [outFileName, '.mat']);
+    [timetamps, TTLs, header] = Nlx2MatEV_v3(inFileNames{i}, [1 0 1 0 0], 1,1,[]);
+    dt = diff(timetamps);
+    inds = find(dt<50 & dt>0);
+    TTLs(inds) = [];
+    timetamps(inds) = [];
+    timetamps = timetamps*1e-6; % convert timestamps to seconds.
+    
+    matobj = matfile(outFileNameTemp, 'Writable', true);
+    matobj.TTLs = TTLs;
+    matobj.timestamps = timetamps;
+    matobj.header = header;
+    
+    movefile(outFileNameTemp, outFileName);
+    outFileNames{i} = outFileName;
+
+end
--- src/utils/unpackData.m
+++ src/utils/unpackData.m
@@ -2,7 +2,7 @@ function outFileNames = unpackData(inFileNames, outFileNames, outFilePath, verbo
 % unpackData(inFileNames, outFilePath, verbose): read neuralynx file and
 % save to .mat files.
 
-% inFileName: datatable(m, 1). '.ncs' files for one experiment. Should have
+% inFileName: datatable(m, n). '.ncs' files for one experiment. Should have
 % same timestamps.
 
 % timestamps: Unix time samplingInterval: matlab duration object.
@@ -22,7 +22,7 @@ if nargin < 5
     skipExist = 1;
 end
 
-makeOutputPath(inFileNames, outFilePath, skipExist)
+makeOutputPath(inFileNames(:), outFilePath, skipExist)
 
 % TO DO: probably don't want to hard code timestamp file name.
 timestampFileName = 'lfpTimeStamps';
@@ -30,37 +30,59 @@ timestampFileName = 'lfpTimeStamps';
 % compute stampstamp for the first channel in each segment.
 % each segment has a suffix with pattern '001'. We need precompute when to
 % cmopute timestamps as deciding within parfor will cause issues.
-suffix = regexp(outFileNames, '(?<=_)\d{3}(?=.mat)', 'match', 'once');
-suffix_int = cellfun(@(x) int8(str2double(x)), suffix);
-[~, computeTS] = findFirstOccurrence(suffix_int);
+suffix = regexp(outFileNames(1,:), '(?<=_)\d{3}(?=.mat)', 'match', 'once');
 
 % unpack ncs files:
-parfor i = 1:length(inFileNames)
-    [~, ~, ext] = fileparts(inFileNames{i});
-    [~, outFileName, ~] = fileparts(outFileNames{i});
-    outFileNameTemp = fullfile(outFilePath, [outFileName, 'temp.mat']);
-    outFileName = fullfile(outFilePath, [outFileName, '.mat']);
+for segment = 1: size(inFileNames, 2)
+    % use non-parallel loop to read timestamps:
+    
+    timestampFullFile = fullfile(outFilePath, [timestampFileName, '_', suffix{segment}]);
+    startIndx = 1;
+    if ~(skipExist && exist(timestampFullFile, "file"))
+        computedTimeStamps = NaN;
+        while isnan(computedTimeStamps) 
+            [signal, ADBitVolts, timestamps, numSamples, samplingInterval, ~] = Nlx_readCSC(inFileNames{startIndx, segment}, outFilePath);
+            computedTimeStamps = computeTimeStamps(timeStamps, numSamples);
 
-    if skipExist && ~computeTS(i) && exist(outFileName, "file") && checkMatFileCorruption(outFileName) 
-        continue
+            if ~isnan(computedTimeStamps)
+                saveTimestamps(timestamps, samplingInterval, timestampFullFile)
+            end
+        end
     end
+    
 
-    if exist(outFileNameTemp, "file")
-        warning('delete temp file: %s\n', outFileNameTemp);
-        delete(outFileNameTemp);
-    end
 
-    if verbose
-        fprintf('unpack: %s\nto: %s\n', inFileNames{i}, outFileName);
-    end
+    parfor i = startIndx:length(inFileNames)
+        [~, ~, ext] = fileparts(inFileNames{i, segment});
 
-    timestampFullFile = fullfile(outFilePath, [timestampFileName, '_', suffix{i}]);
+        if ~strcmp(ext, '.ncs')
+            warning('input file is not ncs!')
+            continue
+        end
+    
+        [~, outFileName, ~] = fileparts(outFileNames{i, segment});
+        outFileNameTemp = fullfile(outFilePath, [outFileName, 'temp.mat']);
+        outFileName = fullfile(outFilePath, [outFileName, '.mat']);
+    
+        if skipExist && ~computeTS && exist(outFileName, "file") && checkMatFileCorruption(outFileName) 
+            continue
+        end
+    
+        if exist(outFileNameTemp, "file")
+            warning('delete temp file: %s\n', outFileNameTemp);
+            delete(outFileNameTemp);
+        end
+    
+        if verbose
+            fprintf('unpack: %s\nto: %s\n', inFileNames{i, segment}, outFileName);
+        end
 
-    if strcmp(ext, '.ncs')
-        [signal, ADBitVolts, timestamps, samplingInterval, ~] = Nlx_readCSC(inFileNames{i}, computeTS(i), outFilePath);
+    
+        [signal, ADBitVolts, ~, ~, samplingInterval, ~] = Nlx_readCSC(inFileNames{i, segment}, outFilePath);
+        
         num_samples = length(signal);
         timeend = (num_samples-1) * samplingInterval;
-
+    
         matobj = matfile(outFileNameTemp, 'Writable', true);
         matobj.samplingInterval = samplingInterval;
         matobj.samplingIntervalSeconds = seconds(samplingInterval);
@@ -69,24 +91,10 @@ parfor i = 1:length(inFileNames)
         matobj.timeend = timeend;
         matobj.timeendSeconds = seconds(timeend);
         matobj.ADBitVolts = ADBitVolts;
+    
 
-        if computeTS(i)
-            saveTimestamps(timestamps, samplingInterval, timestampFullFile)
-        end
-    elseif strcmp(ext, '.nev')
-        [timetamps, TTLs, header] = Nlx2MatEV_v3(inFileNames{i}, [1 0 1 0 0], 1,1,[]);
-        dt = diff(timetamps);
-        inds = find(dt<50 & dt>0);
-        TTLs(inds) = [];
-        timetamps(inds) = [];
-        timetamps = timetamps*1e-6; % convert timestamps to seconds.
-
-        matobj = matfile(outFileNameTemp, 'Writable', true);
-        matobj.TTLs = TTLs;
-        matobj.timestamps = timetamps;
-        matobj.header = header;
+    
+        movefile(outFileNameTemp, outFileName);
+        outFileNames{i, segment} = outFileName;
     end
-
-    movefile(outFileNameTemp, outFileName);
-    outFileNames{i} = outFileName;
 end
