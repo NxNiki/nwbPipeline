8938c6723dca35e9a7926f1154e98f73b0bd8e26
Xin <michaelniki1988@gmail.com>
2024-11-20 19:14:04 -0800

    [feat] move non73 times_manual files to local

--- tools/convert_matfile_version.m
+++ tools/convert_matfile_version.m
@@ -4,14 +4,27 @@
 % search_path = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/567_MovieParadigm/Experiment-6-7-8-9-10/CSC_micro_spikes';
 search_path = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm';
 
+non73_backup_path = '/Users/XinNiuAdmin/Downloads/times_manual_non73_backup';
+
 
 mat_files = dir(fullfile(search_path, "**", "CSC_micro_spikes/times_manual_*.mat"));
 
 for i = 1:length(mat_files)
+    
+    mat_file = fullfile(mat_files(i).folder, mat_files(i).name);
+
     if contains(mat_files(i).name, '_non73.mat')
+        % move to backup path:
+        backup_path = strrep(mat_files(i).folder, search_path, non73_backup_path);
+        if ~exist(backup_path, "dir")
+            mkdir(backup_path);
+        end
+        fprintf('move file: %s\nto %s\n', mat_file, backup_path)
+        movefile(mat_file, fullfile(backup_path, mat_files(i).name));
         continue
     end
-    mat_file = fullfile(mat_files(i).folder, mat_files(i).name);
-    convertToV73(mat_file);
+
+    % convert file to version 7.3:
+    % convertToV73(mat_file);
 end
 
