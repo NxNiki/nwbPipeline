bb32c9ef571e2cd94eede23a72f2bad5e94f90c6
Xin <michaelniki1988@gmail.com>
2024-11-26 17:35:57 -0800

    [FIX!!!] assign channels of a single headstage to the same job

--- batch/splitJobs.m
+++ batch/splitJobs.m
@@ -1,26 +1,31 @@
-function taskIds = splitJobs(numTasks, numWorkers, workerId)
+function taskIds = splitJobs(numTasks, maxNumWorkers, workerId, channelsPerHeadstage)
     % assign n tasks to m workers and return the id of tasks for a specific worker.
     % workerId and taskId starts from 1.
+    % channelsPerHeadstage: we always have 8 mico channels in a headstage, channels in a single headstage should be assigned to
+    % same job so that cross channel spikeCode calculation is done correctly.
 
-    if numWorkers >= numTasks
-        if workerId <= numTasks
-            taskIds = workerId;
-        else
-            taskIds = [];
-        end
+    if nargin < 4
+        channelsPerHeadstage = 8;
+    end
+
+    if mod(numTasks, channelsPerHeadstage) ~= 0
+        warning("number of tasks should be a multiple of %d", channelsPerHeadstage);
+    end
+
+    taskPerWorker = floor(numTasks / maxNumWorkers);
+    taskPerWorker = ceil(taskPerWorker / channelsPerHeadstage) * channelsPerHeadstage;
+    workersNeeded = ceil(numTasks / taskPerWorker);
+
+    if workerId > workersNeeded
+        taskIds = [];
         return
     end
 
-    taskPerWorker = floor(numTasks / numWorkers);
-    extraTasks = mod(numTasks, numWorkers);
-
-    if workerId <= extraTasks
-        taskPerWorker = taskPerWorker + 1;
-        startJobId = taskPerWorker * (workerId - 1) + 1;
-    else
-        startJobId = taskPerWorker * (workerId - 1) + 1 + extraTasks;
-    end
-    
+    startJobId = taskPerWorker * (workerId - 1) + 1;
     taskIds = startJobId: min(startJobId + taskPerWorker - 1, numTasks);
+
+    if mod(length(taskIds) , channelsPerHeadstage) ~= 0
+        warning('number of tasks in each job should be multiple of %d to ensure channels in same headstage are procossed together', channelsPerHeadstage);
+    end
 
 end
