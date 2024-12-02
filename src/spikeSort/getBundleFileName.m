d8f2f27c26b24c109fdfc6a2a06ccd6000a7226c
Xin <michaelniki1988@gmail.com>
2024-12-02 12:43:29 -0800

    [FEAT] calculate bundle median

---
+++ src/spikeSort/getBundleFileName.m
@@ -0,0 +1,7 @@
+function out = getBundleFileName(microFileName)
+
+    [filePath, fileName, ext] = fileparts(microFileName);
+    fileName = regexprep(fileName, '(G[A-D][1-9]-).*(_00[1-9])', ['$1', 'bundleMedian', '$2']);
+    out = fullfile(filePath, 'bundleMedian', [fileName, ext]);
+
+end
---
+++ src/utils/calculateBundleMedian.m
@@ -0,0 +1,47 @@
+function bundleMedianFiles = calculateBundleMedian(microFiles, skipExist)
+
+if nargin < 2
+    skipExist = true;
+end
+
+channelsPerBundle = 8;
+runRemovePLI = false;
+numBundles = ceil(size(microFiles, 1) / channelsPerBundle);
+nSegments = size(microFiles, 2);
+bundleMedianFiles = cell(numBundles, size(microFiles, 2));
+
+parfor i = 1: numBundles
+    for j = 1: nSegments
+        bundleStartIdx = (i-1) * channelsPerBundle + 1;
+        bundleMicroFiles = microFiles(bundleStartIdx:bundleStartIdx+channelsPerBundle-1, j);
+
+        fprintf('process: %s\n', bundleMicroFiles{:});
+        bundleMedianFileName = getBundleFileName(bundleMicroFiles{1});
+
+        if skipExist && exist(bundleMedianFileName, "file")
+            continue;
+        end
+
+        signal = readCSC(bundleMicroFiles{1}, runRemovePLI, true);
+        cscLength = length(signal);
+        bundleMicroCSC = nan(channelsPerBundle, cscLength);
+        bundleMicroCSC(1, :) = signal(:)';
+
+        for k = 2:channelsPerBundle
+            signal = readCSC(bundleMicroFiles{k}, runRemovePLI, true);
+            bundleMicroCSC(k, :) = signal(:)';
+        end
+        
+        bundleMedian = single(nanmedian(bundleMicroCSC, 1));
+        fprintf('save bundle meadian file: %s.\n', bundleMedianFileName);
+        bundleMedianFileNameTemp = strrep(bundleMedianFileName, '.mat', '_temp.mat');
+        if exist(bundleMedianFileNameTemp, "file")
+            delete(bundleMedianFileNameTemp);
+        end
+        bundleMedianFileObj = matfile(bundleMedianFileNameTemp, "Writable", true);
+        bundleMedianFileObj.bundleMedian = bundleMedian(:);
+        movefile(bundleMedianFileNameTemp, bundleMedianFileName)
+    end
+end
+
+end
