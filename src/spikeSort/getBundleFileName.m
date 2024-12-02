97ac0e01b0e8eca2b49cc8cd640c22be5d2117fd
Xin <michaelniki1988@gmail.com>
2024-12-02 12:57:49 -0800

    [fix] make bundleMedian path.

--- src/spikeSort/getBundleFileName.m
+++ src/spikeSort/getBundleFileName.m
@@ -4,4 +4,8 @@ function out = getBundleFileName(microFileName)
     fileName = regexprep(fileName, '(G[A-D][1-9]-).*(_00[1-9])', ['$1', 'bundleMedian', '$2']);
     out = fullfile(filePath, 'bundleMedian', [fileName, ext]);
 
+    if ~exist(fullfile(filePath, 'bundleMedian'), "dir")
+        mkdir(fullfile(filePath, 'bundleMedian'));
+    end
+
 end
