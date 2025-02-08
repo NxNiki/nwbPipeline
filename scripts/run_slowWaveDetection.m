70a444832f44fa8174e9389730d084392ac1e6e4
Xin <michaelniki1988@gmail.com>
2025-02-07 19:35:51 -0800

    [feat] add slow wave detection

---
+++ scripts/run_slowWaveDetection.m
@@ -0,0 +1,54 @@
+%the struct runData holds data about patients and where the different event
+%types are stored
+clear
+% ---- UPDATE this part -
+
+% the main path for extracted data here -
+% for the given example, it's in the same folder as this code:
+
+base_path = '/Users/XinNiuAdmin/HoffmanMount/data/PIPELINE_vc/ANALYSIS/MovieParadigm/561_MovieParadigm/Experiment-4/';
+data_p_path = fullfile(base_path, 'CSC_macro');
+outputFolder = fullfile(base_path, 'Spindles');
+
+if ~exist(outputFolder, "dir")
+    mkdir(outputFolder);
+end
+
+sleepScoringFileName = fullfile(base_path, 'sleep_score/sleepScore_LAC4_001.mat');
+[macroFiles, macroTimestampFiles] = readCSCFilePath(data_p_path);
+channel_index = 25;
+
+%% an example for detecting spindles directly using SpindleDetectorClass (it's the same thing the wrapper below does in batch)
+
+%loading - sleep scoring, IIS, data
+sleepScoring = load(sleepScoringFileName);
+sleepScoring = sleepScoring.sleep_score_vec;
+sleepScoring = [];
+
+%% load or perform interictal Spikes Detection
+
+peakTimes = [];
+channelName = extractChannelName(macroFiles{channel_index, 1});
+currData = combineCSC(macroFiles(channel_index, :), macroTimestampFiles);
+
+%% detect the spindles
+returnStats = 1;
+detector = SlowWavesDetectorClass;
+detector.samplingRate = 2000;
+
+[spindlesTimes, spindleStats, spindlesStartEndTimes] = detector.findSlowWavesStaresina(currData, sleepScoring, peakTimes, returnStats);
+
+matObj = matfile(fullfile(outputFolder, sprintf('spindles_%s.mat', channelName)), "Writable", true);
+matObj.spindlesTimes = spindlesTimes;
+matObj.spindleStats = spindleStats;
+matObj.spindlesStartEndTimes = spindlesStartEndTimes;
+
+%plotting the single spindles and saving the figures
+detector.plotSpindlesSimple(currData, spindlesTimes, outputFolder)
+
+% scroll through spindles and their spectrograms using any key
+blockSize = 4;
+% detector.plotSpindles(currData, spindlesTimes, blockSize);
+
+
+
